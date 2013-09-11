54dbabb4bbe48ad1dd064a9d72489e96d914cdd9
Florian Nücke <florian@nuecke.de>
2013-09-11 21:25:56 +0200

    hooked up client packet handler

--- li/cil/oc/OpenComputers.scala
+++ li/cil/oc/OpenComputers.scala
@@ -1,7 +1,6 @@
 package li.cil.oc
 
 import java.util.logging.Logger
-
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.Mod.EventHandler
 import cpw.mods.fml.common.SidedProxy
@@ -9,10 +8,14 @@ import cpw.mods.fml.common.event.FMLInitializationEvent
 import cpw.mods.fml.common.event.FMLPostInitializationEvent
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
 import cpw.mods.fml.common.network.NetworkMod
+import cpw.mods.fml.common.network.NetworkMod.SidedPacketHandler
 import li.cil.oc.common.CommonProxy
+import li.cil.oc.client.{ PacketHandler => ClientPacketHandler }
 
 @Mod(modid = "OpenComputers", name = "OpenComputers", version = "0.0.0", dependencies = "required-after:Forge@[9.10.0.804,)", modLanguage = "scala")
-@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+@NetworkMod(clientSideRequired = true, serverSideRequired = false,
+  clientPacketHandlerSpec = new SidedPacketHandler(
+    channels = Array("OpenComp"), packetHandler = classOf[ClientPacketHandler]))
 object OpenComputers {
   /** Logger used all throughout this mod. */
   val log = Logger.getLogger("OpenComputers")
