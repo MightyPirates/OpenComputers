60118f19cc4b410d88879b50f33d8d7356d8289c
Florian Nücke <florian@nuecke.de>
2013-10-20 18:34:32 +0200

    lua program can now be quit using 'exit()' function

--- assets/opencomputers/lua/rom/bin/lua.lua
+++ assets/opencomputers/lua/rom/bin/lua.lua
@@ -1,13 +1,15 @@
-while term.isAvailable() do
+print("Lua 5.2.2 Copyright (C) 1994-2013 Lua.org, PUC-Rio")
+local running = true
+local env = setmetatable({exit=function() running = false end}, {__index=_ENV})
+while running and term.isAvailable() do
   io.write("lua> ")
   local command = io.read()
   if not command then
     return -- eof
   end
-
-  local code, result = load("return " .. command, "=stdin")
+  local code, result = load("return " .. command, "=stdin", env)
   if not code then
-    code, result = load(command, "=stdin") -- maybe it's a statement
+    code, result = load(command, "=stdin", env) -- maybe it's a statement
   end
   if code then
     local result = table.pack(pcall(code))
