e837c43cf6267c566c16816d15db18c4c3fd07e8
Florian Nücke <florian@nuecke.de>
2013-11-29 21:35:38 +0100

    lua program to change screen resolution

---
+++ assets/opencomputers/lua/rom/bin/resolution.lua
@@ -0,0 +1,22 @@
+local args = shell.parse(...)
+if #args == 0 then
+  print(component.gpu.getResolution())
+  return
+end
+
+if #args < 2 then
+  print("Usage: resolution [<width> <height>]")
+  return
+end
+
+local w = tonumber(args[1])
+local h = tonumber(args[2])
+if not w or not h then
+  print("invalid width or height")
+  return
+end
+
+local result, reason = component.gpu.setResolution(w, h)
+if not result then
+  print(reason)
+end
