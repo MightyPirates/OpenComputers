c97440dba4d02fd3e6c8578b33a3d114f4c319ed
Johannes Lohrer <johanneslohrer@msn.com>
2013-12-01 00:58:22 +0100

    renamed cards

--- li/cil/oc/common/item/Card.scala
+++ li/cil/oc/common/item/Card.scala
@@ -3,21 +3,8 @@ package li.cil.oc.common.item
 import net.minecraft.item.ItemStack
 
 
-class Card(val parent: Delegator,val tier:Int) extends Delegate {
-  val unlocalizedName = "Card"
+class Card(val parent: Delegator, val tier: Int) extends Delegate {
+  val baseName = "Card"
+  val unlocalizedName = baseName + Array("Basic", "Advanced", "Professional").apply(tier)
 
-  override def displayName(stack: ItemStack) = {
-    if (tier == 0) {
-      Option("Redstone Card")
-    }
-    else if (tier == 1) {
-      Option("Golden Card")
-    }
-    else if (tier == 2) {
-      Option("Diamond Card")
-    }
-    else{
-      Option(unlocalizedName)
-    }
-  }
 }
