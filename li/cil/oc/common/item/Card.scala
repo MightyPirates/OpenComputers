15c178cdf80edadb32b6d18c2ec5aa364fb4b6bf
Florian Nücke <florian@nuecke.de>
2013-12-01 01:48:25 +0100

    card graphic, minor touch-up to existing cards

--- li/cil/oc/common/item/Card.scala
+++ li/cil/oc/common/item/Card.scala
@@ -1,10 +1,15 @@
 package li.cil.oc.common.item
 
-import net.minecraft.item.ItemStack
-
+import li.cil.oc.Settings
+import net.minecraft.client.renderer.texture.IconRegister
 
 class Card(val parent: Delegator, val tier: Int) extends Delegate {
   val baseName = "Card"
   val unlocalizedName = baseName + Array("Basic", "Advanced", "Professional").apply(tier)
 
+  override def registerIcons(iconRegister: IconRegister) {
+    super.registerIcons(iconRegister)
+
+    icon = iconRegister.registerIcon(Settings.resourceDomain + ":card")
+  }
 }
