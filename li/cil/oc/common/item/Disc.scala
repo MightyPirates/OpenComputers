831f997f8b6b4698ca16db8ece14d16b7c68d388
Florian Nücke <florian@nuecke.de>
2013-12-01 02:59:14 +0100

    disc image

--- li/cil/oc/common/item/Disc.scala
+++ li/cil/oc/common/item/Disc.scala
@@ -1,6 +1,14 @@
 package li.cil.oc.common.item
 
+import li.cil.oc.Settings
+import net.minecraft.client.renderer.texture.IconRegister
 
 class Disc(val parent: Delegator) extends Delegate {
   val unlocalizedName = "Disc"
+
+  override def registerIcons(iconRegister: IconRegister) {
+    super.registerIcons(iconRegister)
+
+    icon = iconRegister.registerIcon(Settings.resourceDomain + ":disc")
+  }
 }
