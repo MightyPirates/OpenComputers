10ee625c80c93d0622d43eab90dea32bff90e7d3
Florian Nücke <florian@nuecke.de>
2013-12-01 01:09:17 +0100

    cutting wire image

--- li/cil/oc/common/item/CuttingWire.scala
+++ li/cil/oc/common/item/CuttingWire.scala
@@ -1,5 +1,14 @@
 package li.cil.oc.common.item
 
+import li.cil.oc.Settings
+import net.minecraft.client.renderer.texture.IconRegister
+
 class CuttingWire(val parent: Delegator) extends Delegate {
   val unlocalizedName = "CuttingWire"
+
+  override def registerIcons(iconRegister: IconRegister) {
+    super.registerIcons(iconRegister)
+
+    icon = iconRegister.registerIcon(Settings.resourceDomain + ":cutting_wire")
+  }
 }
