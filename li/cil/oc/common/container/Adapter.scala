11fc10925f142527cb4a346fc6c7daa7248679a3
Florian Nücke <florian@nuecke.de>
2013-12-25 21:40:35 +0100

    adapter gui string; not clearing when trying to insert invalid item in adapter

--- li/cil/oc/common/container/Adapter.scala
+++ li/cil/oc/common/container/Adapter.scala
@@ -3,6 +3,7 @@ package li.cil.oc.common.container
 import li.cil.oc.common.tileentity
 import net.minecraft.entity.player.{EntityPlayer, InventoryPlayer}
 import net.minecraft.inventory.Slot
+import net.minecraft.item.ItemStack
 
 class Adapter(playerInventory: InventoryPlayer, adapter: tileentity.Adapter) extends Player(playerInventory, adapter) {
   addSlotToContainer(new ComponentSlot(otherInventory, 0, 80, 35) {
@@ -15,22 +16,19 @@ class Adapter(playerInventory: InventoryPlayer, adapter: tileentity.Adapter) ext
   addPlayerInventorySlots(8, 84)
 
   override def slotClick(index: Int, mouseClick: Int, holdingShift: Int, player: EntityPlayer) = {
-    if (index == 0) {
-      inventorySlots.get(0) match {
-        case slot: Slot => slot.putStack(null)
-        case _ =>
-      }
-    }
+    if (index == 0) clearIfNecessary(player.inventory.getItemStack)
     super.slotClick(index, mouseClick, holdingShift, player)
   }
 
   override protected def tryTransferStackInSlot(from: Slot, offset: Int, length: Int, intoPlayerInventory: Boolean) = {
-    if (!intoPlayerInventory) {
-      inventorySlots.get(0) match {
-        case slot: Slot => slot.putStack(null)
-        case _ =>
-      }
-    }
+    if (!intoPlayerInventory) clearIfNecessary(from.getStack)
     super.tryTransferStackInSlot(from, offset, length, intoPlayerInventory)
+  }
+
+  private def clearIfNecessary(stack: ItemStack) {
+    inventorySlots.get(0) match {
+      case slot: Slot if stack == null || slot.isItemValid(stack) => slot.putStack(null)
+      case _ =>
+    }
   }
 }
