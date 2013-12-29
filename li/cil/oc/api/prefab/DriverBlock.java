b6053b2581ca1bfbf697c86292492390dd034852
Florian Nücke <florian@nuecke.de>
2013-12-29 18:22:42 +0100

    refactored examples into "prefabs", skeleton implementations of some interfaces that can be extended and take care of some of the repetitive work

---
+++ li/cil/oc/api/prefab/DriverBlock.java
@@ -0,0 +1,65 @@
+package li.cil.oc.api.prefab;
+
+import li.cil.oc.api.Driver;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+/**
+ * If you wish to create a block component for a third-party block, i.e. a block
+ * for which you do not control the tile entity, such as vanially blocks, you
+ * will need a block driver.
+ * <p/>
+ * This prefab allows creating a driver that works for a specified list of item
+ * stacks (to support different blocks with the same id but different metadata
+ * values).
+ * <p/>
+ * Note that if you use this prefab you <em>must instantiate your driver in the
+ * init phase</em>, since it automatically registers itself with OpenComputers.
+ * <p/>
+ * You still have to provide the implementation for creating its environment, if
+ * any.
+ *
+ * @see li.cil.oc.api.network.ManagedEnvironment
+ */
+@SuppressWarnings("UnusedDeclaration")
+public abstract class DriverBlock implements li.cil.oc.api.driver.Block {
+    protected ItemStack[] blocks;
+
+    protected DriverBlock(ItemStack... blocks) {
+        this.blocks = blocks.clone();
+
+        // Make the driver known with OpenComputers. This is required, otherwise
+        // the mod won't know this driver exists. It must be called in the init
+        // phase.
+        Driver.add(this);
+    }
+
+    @Override
+    public boolean worksWith(World world, int x, int y, int z) {
+        return worksWith(world.getBlockId(x, y, z), world.getBlockMetadata(x, y, z));
+    }
+
+    @Override
+    public boolean worksWith(ItemStack stack) {
+        if (stack != null && stack.getItem() instanceof ItemBlock) {
+            ItemBlock reference = (ItemBlock) stack.getItem();
+            return worksWith(reference.getBlockID(), reference.getMetadata(stack.getItemDamage()));
+        }
+        return false;
+    }
+
+    protected boolean worksWith(int referenceId, int referenceMetadata) {
+        for (ItemStack block : blocks) {
+            if (block != null && block.getItem() instanceof ItemBlock) {
+                ItemBlock itemBlock = (ItemBlock) block.getItem();
+                int id = itemBlock.getBlockID();
+                int metadata = itemBlock.getMetadata(block.getItemDamage());
+                if (referenceId == id && referenceMetadata == metadata) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ li/cil/oc/api/prefab/DriverItem.java
@@ -0,0 +1,62 @@
+package li.cil.oc.api.prefab;
+
+import li.cil.oc.api.Driver;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * If you wish to create item components such as the network card or hard drives
+ * you will need an item driver.
+ * <p/>
+ * This prefab allows creating a driver that works for a specified list of item
+ * stacks (to support different items with the same id but different damage
+ * values). It also takes care of creating and getting the tag compound on an
+ * item stack to save data to or load data from.
+ * <p/>
+ * Note that if you use this prefab you <em>must instantiate your driver in the
+ * init phase</em>, since it automatically registers itself with OpenComputers.
+ * <p/>
+ * You still have to specify your component's slot type and provide the
+ * implementation for creating its environment, if any.
+ *
+ * @see li.cil.oc.api.network.ManagedEnvironment
+ */
+@SuppressWarnings("UnusedDeclaration")
+public abstract class DriverItem implements li.cil.oc.api.driver.Item {
+    protected ItemStack[] items;
+
+    protected DriverItem(ItemStack... items) {
+        this.items = items.clone();
+
+        // Make the driver known with OpenComputers. This is required, otherwise
+        // the mod won't know this driver exists. It must be called in the init
+        // phase.
+        Driver.add(this);
+    }
+
+    @Override
+    public boolean worksWith(ItemStack stack) {
+        if (stack != null) {
+            for (ItemStack item : items) {
+                if (item != null && item.isItemEqual(stack)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public NBTTagCompound dataTag(ItemStack stack) {
+        if (!stack.hasTagCompound()) {
+            stack.setTagCompound(new NBTTagCompound("tag"));
+        }
+        NBTTagCompound nbt = stack.getTagCompound();
+        // This is the suggested key under which to store item component data.
+        // You are free to change this as you please.
+        if (!nbt.hasKey("oc:data")) {
+            nbt.setCompoundTag("oc:data", new NBTTagCompound());
+        }
+        return nbt.getCompoundTag("oc:data");
+    }
+}
