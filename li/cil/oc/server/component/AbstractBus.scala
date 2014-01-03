8c3e21928da63c96a62ec2f948ab723305a62800
Florian Nücke <florian@nuecke.de>
2014-01-03 17:30:19 +0100

    abstract bus packet queue serialization

--- li/cil/oc/server/component/AbstractBus.scala
+++ li/cil/oc/server/component/AbstractBus.scala
@@ -3,7 +3,8 @@ package li.cil.oc.server.component
 import li.cil.oc.api
 import li.cil.oc.api.network.{Arguments, Context, LuaCallback, Visibility}
 import li.cil.oc.common.tileentity
-import net.minecraft.nbt.NBTTagCompound
+import li.cil.oc.util.ExtendedNBT._
+import net.minecraft.nbt.{NBTTagString, NBTTagList, NBTTagCompound}
 import scala.collection.convert.WrapAsScala._
 import scala.collection.mutable
 import stargatetech2.api.StargateTechAPI
@@ -20,7 +21,7 @@ class AbstractBus(val owner: tileentity.Computer) extends ManagedComponent with 
 
   protected var address = 0
 
-  protected var sendQueue = mutable.Queue.empty[BusPacketLIP]
+  protected val sendQueue = mutable.Queue.empty[QueuedPacket]
 
   // ----------------------------------------------------------------------- //
 
@@ -33,7 +34,16 @@ class AbstractBus(val owner: tileentity.Computer) extends ManagedComponent with 
     owner.signal("bus_message", Int.box(packet.getProtocolID), Int.box(packet.getSender), Int.box(packet.getTarget), data)
   }
 
-  def getNextPacketToSend = if (sendQueue.nonEmpty) sendQueue.dequeue() else null
+  def getNextPacketToSend = if (sendQueue.nonEmpty) {
+    val info = sendQueue.dequeue()
+    val packet = new BusPacketLIP(info.sender, info.target)
+    for ((key, value) <- info.data) {
+      packet.set(key, value)
+    }
+    // TODO generate metadata?
+    packet.finish()
+    packet
+  } else null
 
   def isInterfaceEnabled = isEnabled
 
@@ -63,12 +73,7 @@ class AbstractBus(val owner: tileentity.Computer) extends ManagedComponent with 
   def send(context: Context, args: Arguments): Array[AnyRef] = {
     val target = args.checkInteger(0) & 0xFFFF
     val data = args.checkTable(1)
-    val packet = new BusPacketLIP(address.toShort, target.toShort)
-    for ((key, value) <- data) {
-      packet.set(key.toString, value.toString)
-    }
-    // TODO generate metadata
-    sendQueue += packet
+    sendQueue += new QueuedPacket(address.toShort, target.toShort, Map(data.toSeq.map(entry => (entry._1.toString, entry._2.toString)): _*))
     result(true)
   }
 
@@ -79,7 +84,9 @@ class AbstractBus(val owner: tileentity.Computer) extends ManagedComponent with 
     busInterface.readFromNBT(nbt, "bus")
     isEnabled = nbt.getBoolean("enabled")
     address = nbt.getInteger("address") & 0xFFFF
-    // TODO load queue
+    if (nbt.hasKey("queue")) {
+      sendQueue ++= nbt.getTagList("queue").map(QueuedPacket.readFromNBT)
+    }
   }
 
   override def save(nbt: NBTTagCompound) {
@@ -87,6 +94,39 @@ class AbstractBus(val owner: tileentity.Computer) extends ManagedComponent with 
     busInterface.writeToNBT(nbt, "bus")
     nbt.setBoolean("enabled", isEnabled)
     nbt.setInteger("address", address)
-    // TODO save queue
+    if (sendQueue.nonEmpty) {
+      nbt.setNewTagList("queue", sendQueue.toIterable.map(packet => packet.writeToNBT(new NBTTagCompound())))
+    }
   }
+
+  protected class QueuedPacket(val sender: Short, val target: Short, val data: Map[String, String]) {
+    def writeToNBT(nbt: NBTTagCompound) = {
+      nbt.setShort("sender", sender)
+      nbt.setShort("target", target)
+      val list = new NBTTagList()
+      for ((key, value) <- data) {
+        list.append(key)
+        list.append(value)
+      }
+      nbt.setTag("data", list)
+      nbt
+    }
+  }
+
+  protected object QueuedPacket {
+    def readFromNBT(nbt: NBTTagCompound) = {
+      val sender = nbt.getShort("sender")
+      val target = nbt.getShort("target")
+      val list = nbt.getTagList("data")
+      val data = mutable.Map.empty[String, String]
+      for (i <- 0 until list.tagCount by 2) {
+        (list.tagAt(i), list.tagAt(i + 1)) match {
+          case (key: NBTTagString, value: NBTTagString) => data += key.data -> value.data
+          case _ =>
+        }
+      }
+      new QueuedPacket(sender, target, data.toMap)
+    }
+  }
+
 }
