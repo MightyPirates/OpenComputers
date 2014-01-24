19b95a80d3a9fe8e2b4e901f41e1824d483bccd8
PixelToast <andre.lipke@gmail.com>
2014-01-24 12:08:03 -0500

    forgot one

--- assets/opencomputers/lua/rom/lib/text.lua
+++ assets/opencomputers/lua/rom/lib/text.lua
@@ -43,7 +43,7 @@ end
 function text.serialize(value)
   local kw =  {["and"]=true, ["break"]=true, ["do"]=true, ["else"]=true,
                ["elseif"]=true, ["end"]=true, ["false"]=true, ["for"]=true,
-               ["function"]=true, ["goto "]=true, ["if"]=true, ["in"]=true,
+               ["function"]=true, ["goto"]=true, ["if"]=true, ["in"]=true,
                ["local"]=true, ["nil"]=true, ["not"]=true, ["or"]=true,
                ["repeat"]=true, ["return"]=true, ["then"]=true, ["true"]=true,
                ["until"]=true, ["while"]=true}
