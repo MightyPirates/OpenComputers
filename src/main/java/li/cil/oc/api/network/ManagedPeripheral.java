e2f1172dafe503823535c7420127b8027ed4a8e7
Florian Nücke <florian@nuecke.de>
2014-02-08 00:54:21 +0100

    added an interface to the api that allows custom declarations of methods (aside from the callback annotation), meant to make wrapping cc peripherals easier

---
+++ src/main/java/li/cil/oc/api/network/ManagedPeripheral.java
@@ -0,0 +1,35 @@
+package li.cil.oc.api.network;
+
+/**
+ * This specialization of the managed environment is intended to be used for
+ * environments wrapping a ComputerCraft peripheral, although it could be used
+ * for other purposes as well. It allows providing method names in addition to
+ * those defined via the {@link li.cil.oc.api.network.Callback} annotation, and
+ * invoking said methods.
+ */
+public interface ManagedPeripheral extends ManagedEnvironment {
+    /**
+     * Get the list of methods provided by this environment, in
+     * <em>addition</em> to methods marked as callbacks.
+     * <p/>
+     * Returning <tt>null</tt> has the same meaning as returning an empty array,
+     * that being that there are no methods - in which case you don't need this
+     * interface!
+     *
+     * @return the list of methods provided by the environment.
+     */
+    String[] methods();
+
+    /**
+     * Calls a method from the list provided by {@link #methods()}.
+     * <p/>
+     *
+     * @param method  the name of the method to call.
+     * @param context the context from which the method is called.
+     * @param args    the arguments to pass to the method.
+     * @return the result of calling the method. Same as for callbacks.
+     * @throws java.lang.NoSuchMethodException if there is no method with the
+     *                                         specified name.
+     */
+    Object[] invoke(String method, Context context, Arguments args) throws Exception;
+}
