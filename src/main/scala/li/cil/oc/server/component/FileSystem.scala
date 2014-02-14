40fd6f6332acca0e67199c422f620bd42d73e4b2
Florian Nücke <florian@nuecke.de>
2014-02-14 13:25:12 +0100

    minor cleanup and appending '-devNNN' to version in mcmod.info (for jenkins)

--- src/main/scala/li/cil/oc/server/component/FileSystem.scala
+++ src/main/scala/li/cil/oc/server/component/FileSystem.scala
@@ -237,7 +237,7 @@ class FileSystem(val fileSystem: IFileSystem, var label: Label) extends ManagedC
       val address = ownerNbt.getString("address")
       if (address != "") {
         // Was tag list in 1.6, so wee need to check this when upgrading.
-        if (ownerNbt.getTag("handles").getId == NBT.TAG_INT_ARRAY) {
+        if (ownerNbt.hasKey("handles", NBT.TAG_INT_ARRAY)) {
           owners += address -> ownerNbt.getIntArray("handles").to[mutable.Set]
         }
       }
