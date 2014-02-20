c7bd5fa05e1f44b152a7e397cdd4aa3d9d429fff
Wobbo <rene.mellema@gmail.com>
2014-02-20 22:16:39 +0100

    Added positional parameters

--- src/main/resources/assets/opencomputers/lua/rom/bin/set.lua
+++ src/main/resources/assets/opencomputers/lua/rom/bin/set.lua
@@ -5,14 +5,19 @@ if #args < 1 then
     print(k..'='..v)
   end
 else
-  local count = 1
+  local count = 0 
   for _, expr in ipairs(args) do
     local k, v = string.match(expr, "(.-)=(.*)")
     if v then
       os.setenv(k, v)
     else
-      os.setenv(tostring(count), k)
+      if count == 0 then
+        for i = 1, os.getenv('#') do
+          os.setenv(i, nil)
+        end
+      end
       count = count + 1
+      os.setenv(count, expr)
     end
   end
 end
