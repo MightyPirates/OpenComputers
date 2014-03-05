362e79f779f85618c57fd6b73ea9509e4419a39e
Florian Nücke <florian@nuecke.de>
2014-03-05 15:50:40 +0100

    don't show our own stuff via the adapter

--- src/main/java/li/cil/oc/common/tileentity/Adapter.scala
+++ src/main/java/li/cil/oc/common/tileentity/Adapter.scala
@@ -34,37 +34,46 @@ class Adapter extends Environment with Analyzable {
   def neighborChanged() = if (node != null && node.network != null) {
     for (d <- ForgeDirection.VALID_DIRECTIONS) {
       val (x, y, z) = (this.x + d.offsetX, this.y + d.offsetY, this.z + d.offsetZ)
-      driver.Registry.blockDriverFor(world, x, y, z) match {
-        case Some(newDriver) => blocks(d.ordinal()) match {
-          case Some((oldEnvironment, driver)) =>
-            if (newDriver != driver) {
-              // This is... odd. Maybe moved by some other mod?
-              node.disconnect(oldEnvironment.node)
-              val environment = newDriver.createEnvironment(world, x, y, z)
-              blocks(d.ordinal()) = Some((environment, newDriver))
-              blocksData(d.ordinal()) = Some(new BlockData(environment.getClass.getName, new NBTTagCompound()))
-              node.connect(environment.node)
-            } // else: the more things change, the more they stay the same.
-          case _ =>
-            // A challenger appears.
-            val environment = newDriver.createEnvironment(world, x, y, z)
-            blocks(d.ordinal()) = Some((environment, newDriver))
-            blocksData(d.ordinal()) match {
-              case Some(data) if data.name == environment.getClass.getName =>
-                environment.load(data.data)
+      world.getBlockTileEntity(x, y, z) match {
+        case env: Environment =>
+        // Don't provide adaption for our stuffs. This is mostly to avoid
+        // cables and other non-functional stuff popping up in the adapter
+        // due to having a power interface. Might revisit this at some point,
+        // but the only 'downside' is that it can't be used to manipulate
+        // inventories, which I actually consider a plus :P
+        case _ =>
+          driver.Registry.blockDriverFor(world, x, y, z) match {
+            case Some(newDriver) => blocks(d.ordinal()) match {
+              case Some((oldEnvironment, driver)) =>
+                if (newDriver != driver) {
+                  // This is... odd. Maybe moved by some other mod?
+                  node.disconnect(oldEnvironment.node)
+                  val environment = newDriver.createEnvironment(world, x, y, z)
+                  blocks(d.ordinal()) = Some((environment, newDriver))
+                  blocksData(d.ordinal()) = Some(new BlockData(environment.getClass.getName, new NBTTagCompound()))
+                  node.connect(environment.node)
+                } // else: the more things change, the more they stay the same.
               case _ =>
+                // A challenger appears.
+                val environment = newDriver.createEnvironment(world, x, y, z)
+                blocks(d.ordinal()) = Some((environment, newDriver))
+                blocksData(d.ordinal()) match {
+                  case Some(data) if data.name == environment.getClass.getName =>
+                    environment.load(data.data)
+                  case _ =>
+                }
+                blocksData(d.ordinal()) = Some(new BlockData(environment.getClass.getName, new NBTTagCompound()))
+                node.connect(environment.node)
             }
-            blocksData(d.ordinal()) = Some(new BlockData(environment.getClass.getName, new NBTTagCompound()))
-            node.connect(environment.node)
-        }
-        case _ => blocks(d.ordinal()) match {
-          case Some((environment, driver)) =>
-            // We had something there, but it's gone now...
-            node.disconnect(environment.node)
-            environment.save(blocksData(d.ordinal()).get.data)
-            blocks(d.ordinal()) = None
-          case _ => // Nothing before, nothing now.
-        }
+            case _ => blocks(d.ordinal()) match {
+              case Some((environment, driver)) =>
+                // We had something there, but it's gone now...
+                node.disconnect(environment.node)
+                environment.save(blocksData(d.ordinal()).get.data)
+                blocks(d.ordinal()) = None
+              case _ => // Nothing before, nothing now.
+            }
+          }
       }
     }
   }
