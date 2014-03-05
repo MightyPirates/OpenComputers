e416ad34d0908e9f28b825c9ff5fefccec0e3d90
Florian Nücke <florian@nuecke.de>
2014-03-05 14:47:21 +0100

    well that's lovely, why didn't that error before...

--- src/main/java/li/cil/oc/server/driver/item/UpgradeGenerator.scala
+++ src/main/java/li/cil/oc/server/driver/item/UpgradeGenerator.scala
@@ -2,6 +2,7 @@ package li.cil.oc.server.driver.item
 
 import li.cil.oc.Items
 import li.cil.oc.api.driver.Slot
+import li.cil.oc.api.machine.Robot
 import li.cil.oc.server.component
 import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
@@ -9,7 +10,9 @@ import net.minecraft.tileentity.TileEntity
 object UpgradeGenerator extends Item {
   override def worksWith(stack: ItemStack) = isOneOf(stack, Items.upgradeGenerator)
 
-  override def createEnvironment(stack: ItemStack, container: TileEntity) = new component.UpgradeGenerator(container)
+  override def createEnvironment(stack: ItemStack, container: TileEntity) = container match {
+    case robot: Robot => new component.UpgradeGenerator(robot)
+  }
 
   override def slot(stack: ItemStack) = Slot.Upgrade
 }
