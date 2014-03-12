4967b791268009a7c77847dd889478fc07f9100c
Florian Nücke <florian@nuecke.de>
2014-03-12 05:33:01 +0100

    adjusted sound stuff to the 1.7 way

--- src/main/scala/li/cil/oc/client/Sound.scala
+++ src/main/scala/li/cil/oc/client/Sound.scala
@@ -9,25 +9,35 @@ import net.minecraft.client.Minecraft
 import net.minecraft.tileentity.TileEntity
 import net.minecraftforge.client.event.sound.SoundLoadEvent
 import net.minecraftforge.event.world.{WorldEvent, ChunkEvent}
-import paulscode.sound.SoundSystemConfig
+import paulscode.sound.{SoundSystem, SoundSystemConfig}
 import scala.collection.convert.WrapAsScala._
 import scala.collection.mutable
+import net.minecraft.client.audio.{SoundCategory, SoundManager}
+import cpw.mods.fml.relauncher.ReflectionHelper
+import net.minecraft.util.ResourceLocation
+import java.net.{MalformedURLException, URLConnection, URLStreamHandler, URL}
 
 object Sound {
   val sources = mutable.Map.empty[TileEntity, (String, Float)]
 
-  var lastVolume = FMLClientHandler.instance.getClient.gameSettings.soundVolume
+  var lastVolume = FMLClientHandler.instance.getClient.gameSettings.getSoundLevel(SoundCategory.BLOCKS)
+
+  // Set in init event.
+  var manager: SoundManager = _
+
+  def handler = Minecraft.getMinecraft.getSoundHandler
+
+  def soundSystem: SoundSystem = ReflectionHelper.getPrivateValue(classOf[SoundManager], manager, "sndSystem")
 
   val volumeCheckTimer = new Timer("OpenComputers-VolumeUpdater", true)
   volumeCheckTimer.scheduleAtFixedRate(new TimerTask {
     override def run() {
-      val volume = FMLClientHandler.instance.getClient.gameSettings.soundVolume
+      val volume = FMLClientHandler.instance.getClient.gameSettings.getSoundLevel(SoundCategory.BLOCKS)
       if (volume != lastVolume) {
         lastVolume = volume
-        val system = Minecraft.getMinecraft.sndManager.sndSystem
         sources.synchronized {
           for ((source, volume) <- sources.values) {
-            system.setVolume(source, lastVolume * volume * Settings.get.soundVolume)
+            soundSystem.setVolume(source, lastVolume * volume * Settings.get.soundVolume)
           }
         }
       }
@@ -37,17 +47,17 @@ object Sound {
   def startLoop(tileEntity: TileEntity, name: String, volume: Float = 1f) {
     if (Settings.get.soundVolume > 0) {
       val resourceName = s"${Settings.resourceDomain}:$name"
-      val manager = Minecraft.getMinecraft.getSoundHandler
-      val sound = manager.soundPoolSounds.getRandomSoundFromSoundPool(resourceName)
+      val sound = Minecraft.getMinecraft.getSoundHandler.getSound(new ResourceLocation(resourceName))
+      val resource = sound.func_148720_g.getSoundPoolEntryLocation
       sources.synchronized {
         val (source, _) = sources.getOrElseUpdate(tileEntity, {
           val source = UUID.randomUUID.toString
-          manager.sndSystem.newStreamingSource(false, source, sound.getSoundUrl, sound.getSoundName, true, tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord, SoundSystemConfig.ATTENUATION_LINEAR, 16.0f)
-          manager.sndSystem.setVolume(source, lastVolume * volume * Settings.get.soundVolume)
+          soundSystem.newStreamingSource(false, source, toUrl(resource), resource.toString, true, tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord, SoundSystemConfig.ATTENUATION_LINEAR, 16.0f)
+          soundSystem.setVolume(source, lastVolume * volume * Settings.get.soundVolume)
           (source, volume)
         })
-        manager.sndSystem.fadeOutIn(source, sound.getSoundUrl, sound.getSoundName, 50, 500)
-        manager.sndSystem.play(source)
+        soundSystem.fadeOutIn(source, toUrl(resource), resource.toString, 50, 500)
+        soundSystem.play(source)
       }
     }
   }
@@ -55,8 +65,7 @@ object Sound {
   def updatePosition(tileEntity: TileEntity) {
     sources.synchronized {
       sources.get(tileEntity) match {
-        case Some((source, _)) => Minecraft.getMinecraft.sndManager.sndSystem.
-          setPosition(source, tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord)
+        case Some((source, _)) => soundSystem.setPosition(source, tileEntity.xCoord, tileEntity.yCoord, tileEntity.zCoord)
         case _ =>
       }
     }
@@ -65,7 +74,7 @@ object Sound {
   def stopLoop(tileEntity: TileEntity) {
     sources.synchronized {
       sources.get(tileEntity) match {
-        case Some((source, _)) => Minecraft.getMinecraft.sndManager.sndSystem.fadeOut(source, null, 500)
+        case Some((source, _)) => soundSystem.fadeOut(source, null, 500)
         case _ =>
       }
     }
@@ -73,16 +82,7 @@ object Sound {
 
   @SubscribeEvent
   def onSoundLoad(event: SoundLoadEvent) {
-    for (i <- 1 to 6) {
-      event.manager.soundPoolSounds.addSound(Settings.resourceDomain + s":floppy_access$i.ogg")
-    }
-    for (i <- 1 to 7) {
-      event.manager.soundPoolSounds.addSound(Settings.resourceDomain + s":hdd_access$i.ogg")
-    }
-    event.manager.soundPoolSounds.addSound(Settings.resourceDomain + ":floppy_insert.ogg")
-    event.manager.soundPoolSounds.addSound(Settings.resourceDomain + ":floppy_eject.ogg")
-
-    event.manager.soundPoolSounds.addSound(Settings.resourceDomain + ":computer_running.ogg")
+    manager = event.manager
   }
 
   @SubscribeEvent
@@ -96,23 +96,40 @@ object Sound {
   }
 
   def cleanup[_](list: Iterable[_]) {
-    val system = Minecraft.getMinecraft.sndManager.sndSystem
     sources.synchronized {
       list.foreach {
         case robot: tileentity.RobotProxy => sources.remove(robot.robot) match {
           case Some((source, _)) =>
-            system.stop(source)
-            system.removeSource(source)
+            soundSystem.stop(source)
+            soundSystem.removeSource(source)
           case _ =>
         }
         case tileEntity: TileEntity => sources.remove(tileEntity) match {
           case Some((source, _)) =>
-            system.stop(source)
-            system.removeSource(source)
+            soundSystem.stop(source)
+            soundSystem.removeSource(source)
           case _ =>
         }
         case _ =>
       }
+    }
+  }
+
+  // This is copied from SoundManager.getURLForSoundResource, which is private.
+  private def toUrl(resource: ResourceLocation): URL = {
+    val name = s"mcsounddomain:${resource.getResourceDomain}:${resource.getResourcePath}"
+    try {
+      new URL(null, name, new URLStreamHandler {
+        protected def openConnection(url: URL): URLConnection = new URLConnection(url) {
+          def connect() {
+          }
+
+          override def getInputStream = Minecraft.getMinecraft.getResourceManager.getResource(resource).getInputStream
+        }
+      })
+    }
+    catch {
+      case _: MalformedURLException => null
     }
   }
 }
