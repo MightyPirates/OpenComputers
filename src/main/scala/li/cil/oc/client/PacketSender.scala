f384eadecaf0e3309d21fef3c54f0383789ebd81
Florian Nücke <florian@nuecke.de>
2014-04-04 20:30:19 +0200

    Relaxed throttling of clipboard pasting a little and added audio feedback when pasting while on cooldown.

--- src/main/scala/li/cil/oc/client/PacketSender.scala
+++ src/main/scala/li/cil/oc/client/PacketSender.scala
@@ -3,6 +3,7 @@ package li.cil.oc.client
 import li.cil.oc.common.tileentity._
 import li.cil.oc.common.tileentity.traits.{Computer, TextBuffer}
 import li.cil.oc.common.{CompressedPacketBuilder, PacketBuilder, PacketType, component}
+import net.minecraft.client.Minecraft
 import net.minecraftforge.common.ForgeDirection
 
 object PacketSender {
@@ -54,21 +55,26 @@ object PacketSender {
   }
 
   def sendClipboard(b: component.Buffer, value: String) {
-    if (value != null && !value.isEmpty && System.currentTimeMillis() > clipboardCooldown) {
-      clipboardCooldown = System.currentTimeMillis() + value.length
-      val pb = new CompressedPacketBuilder(PacketType.Clipboard)
-
-      b.owner match {
-        case t: TextBuffer if t.hasKeyboard =>
-          pb.writeTileEntity(t)
-        case t: component.Terminal =>
-          pb.writeTileEntity(t.rack)
-          pb.writeInt(t.number)
-        case _ => return
+    if (value != null && !value.isEmpty) {
+      if (System.currentTimeMillis() < clipboardCooldown) {
+        Minecraft.getMinecraft.sndManager.playSoundFX("note.harp", 3, 1)
       }
-      pb.writeUTF(value.substring(0, math.min(value.length, 64 * 1024)))
+      else {
+        clipboardCooldown = System.currentTimeMillis() + value.length / 10
+        val pb = new CompressedPacketBuilder(PacketType.Clipboard)
 
-      pb.sendToServer()
+        b.owner match {
+          case t: TextBuffer if t.hasKeyboard =>
+            pb.writeTileEntity(t)
+          case t: component.Terminal =>
+            pb.writeTileEntity(t.rack)
+            pb.writeInt(t.number)
+          case _ => return
+        }
+        pb.writeUTF(value.substring(0, math.min(value.length, 64 * 1024)))
+
+        pb.sendToServer()
+      }
     }
   }
 
