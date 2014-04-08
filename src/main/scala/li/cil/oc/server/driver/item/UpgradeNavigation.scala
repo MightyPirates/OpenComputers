acff869e9a253bcb4185e162e70fa48519048df1
Florian Nücke <florian@nuecke.de>
2014-04-08 12:45:26 +0200

    cleanup

--- src/main/scala/li/cil/oc/server/driver/item/UpgradeNavigation.scala
+++ src/main/scala/li/cil/oc/server/driver/item/UpgradeNavigation.scala
@@ -2,7 +2,6 @@ package li.cil.oc.server.driver.item
 
 import li.cil.oc.api.driver.Slot
 import li.cil.oc.server.component
-import li.cil.oc.server.driver.Registry
 import li.cil.oc.Items
 import net.minecraft.item.ItemStack
 import net.minecraft.tileentity.TileEntity
@@ -10,13 +9,8 @@ import net.minecraft.tileentity.TileEntity
 object UpgradeNavigation extends Item {
   override def worksWith(stack: ItemStack) = isOneOf(stack, Items.upgradeNavigation)
 
-  override def createEnvironment(stack: ItemStack, container: TileEntity) = {
-    val nbt = Registry.itemDriverFor(stack) match {
-      case Some(driver) => driver.dataTag(stack)
-      case _ => null
-    }
+  override def createEnvironment(stack: ItemStack, container: TileEntity) =
     new component.UpgradeNavigation(container)
-  }
 
   override def slot(stack: ItemStack) = Slot.Upgrade
 }
