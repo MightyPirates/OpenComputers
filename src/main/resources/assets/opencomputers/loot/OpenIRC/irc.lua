b22af6a55bdd56af6ed681ea644d99b072618975
Josh <JoshTheEnder@users.noreply.github.com>
2014-04-24 11:05:10 +0100

    Update irc.lua

--- src/main/resources/assets/opencomputers/loot/OpenIRC/irc.lua
+++ src/main/resources/assets/opencomputers/loot/OpenIRC/irc.lua
@@ -414,7 +414,7 @@ local result, reason = pcall(function()
           print("No default target set. Use /msg or /join to set one.")
           line = ""
         else
-          line = "PRIVMSG " .. target .. " :\001ACTION\001 " .. line
+          line = "PRIVMSG " .. target .. " :\001ACTION\001 " .. line:sub(5)
         end
       elseif line:sub(1, 1) == "/" then
         line = line:sub(2)
