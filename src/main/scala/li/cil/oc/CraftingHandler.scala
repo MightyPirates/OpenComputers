a579a3792f251b8b40fc257fbdc34069940560e2
Florian Nücke <florian@nuecke.de>
2014-05-16 00:48:51 +0200

    Removed the bucket consumption in acid/grog crafting and bucket retrieval in PCB crafting. Kind of meh because the grog now is in a magical container that disappears instead of the bucket, logically speaking, but it avoids issues - in particular this closes #194.

--- src/main/scala/li/cil/oc/CraftingHandler.scala
+++ src/main/scala/li/cil/oc/CraftingHandler.scala
@@ -9,28 +9,6 @@ import net.minecraft.item.{ItemMap, Item, ItemStack}
 
 object CraftingHandler extends ICraftingHandler {
   override def onCrafting(player: EntityPlayer, craftedStack: ItemStack, inventory: IInventory) = {
-    if (craftedStack.isItemEqual(Items.acid.createItemStack())) {
-      for (i <- 0 until inventory.getSizeInventory) {
-        val stack = inventory.getStackInSlot(i)
-        if (stack != null && stack.getItem == Item.bucketWater) {
-          stack.stackSize = 0
-          inventory.setInventorySlotContents(i, null)
-        }
-      }
-    }
-
-    if (craftedStack.isItemEqual(Items.pcb.createItemStack())) {
-      for (i <- 0 until inventory.getSizeInventory) {
-        val stack = inventory.getStackInSlot(i)
-        if (stack != null && stack.isItemEqual(Items.acid.createItemStack())) {
-          val container = new ItemStack(Item.bucketEmpty, 1)
-          if (!player.inventory.addItemStackToInventory(container)) {
-            player.dropPlayerItem(container)
-          }
-        }
-      }
-    }
-
     if (craftedStack.isItemEqual(Items.upgradeNavigation.createItemStack())) {
       Registry.itemDriverFor(craftedStack) match {
         case Some(driver) =>
