8ce9d5d620b53f0802ee53d7e74bf355847f5a76
Florian Nücke <florian@nuecke.de>
2014-06-13 23:41:59 +0200

    Should fix #323.

--- src/main/scala/li/cil/oc/common/tileentity/Disassembler.scala
+++ src/main/scala/li/cil/oc/common/tileentity/Disassembler.scala
@@ -15,6 +15,7 @@ import scala.collection.convert.WrapAsScala._
 import scala.collection.mutable
 import li.cil.oc.common.inventory.ServerInventory
 import java.util.logging.Level
+import li.cil.oc.common.InventorySlots.Tier
 
 class Disassembler extends traits.Environment with traits.PowerAcceptor with traits.Inventory {
   val node = api.Network.newNode(this, Visibility.None).
@@ -91,7 +92,10 @@ class Disassembler extends traits.Environment with traits.PowerAcceptor with tra
 
   private def enqueueRobot(robot: ItemStack) {
     val info = new ItemUtils.RobotData(robot)
-    queue += api.Items.get("case" + (info.tier + 1)).createItemStack(1)
+    val itemName =
+      if (info.tier == Tier.Four) "caseCreative"
+      else "case" + (info.tier + 1)
+    queue += api.Items.get(itemName).createItemStack(1)
     queue ++= info.containers
     queue ++= info.components
     node.changeBuffer(info.robotEnergy)
