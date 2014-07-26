0669163ea31113f4058835c47e2ef70bd61f5614
Johannes Lohrer <johanneslohrer@msn.com>
2014-07-26 14:00:21 +0200

    more cleaning...

--- src/api/java/thaumcraft/api/research/IScanEventHandler.java
+++
@@ -1,9 +0,0 @@
-package thaumcraft.api.research;
-
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
-
-public interface IScanEventHandler {
-	ScanResult scanPhenomena(ItemStack stack, World world, EntityPlayer player);
-}
