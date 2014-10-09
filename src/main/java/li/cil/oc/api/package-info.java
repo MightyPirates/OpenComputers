34249f713178f5edcaa4ad9bcfd3946e15c23940
Florian Nücke <florian@nuecke.de>
2014-10-09 18:59:48 +0200

    API stuffs.

--- src/main/java/li/cil/oc/api/package-info.java
+++ src/main/java/li/cil/oc/api/package-info.java
@@ -36,6 +36,6 @@
  */
 @cpw.mods.fml.common.API(
         owner = "OpenComputers|Core",
-        provides = "OpenComputersAPI",
+        provides = "OpenComputersAPI|Core",
         apiVersion = "3.0.0")
 package li.cil.oc.api;
