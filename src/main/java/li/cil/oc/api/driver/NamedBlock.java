7faa309388b6bc1021709287e05af49264ad55cd
Florian Nücke <florian@nuecke.de>
2014-11-03 14:36:43 +0100

    Fixes #645.

--- src/main/java/li/cil/oc/api/driver/NamedBlock.java
+++ src/main/java/li/cil/oc/api/driver/NamedBlock.java
@@ -13,10 +13,6 @@ package li.cil.oc.api.driver;
 public interface NamedBlock {
     /**
      * The preferred name, in case the driver is merged with others.
-     * <p/>
-     * If multiple drivers with a preferred name are merged, the first one is
-     * picked. This should usually not happen, since this is only intended to
-     * be implemented by drivers for actual tile entities (not interfaces).
      *
      * @return the preferred name.
      */
