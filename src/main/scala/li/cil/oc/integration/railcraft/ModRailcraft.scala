814b4ed5bc36a784007da1e1f4e42a78a724582d
Florian Nücke <florian@nuecke.de>
2014-11-27 14:42:54 +0100

    Added support for railcraft crowbars.

--- src/main/scala/li/cil/oc/integration/railcraft/ModRailcraft.scala
+++ src/main/scala/li/cil/oc/integration/railcraft/ModRailcraft.scala
@@ -1,5 +1,6 @@
 package li.cil.oc.integration.railcraft
 
+import cpw.mods.fml.common.event.FMLInterModComms
 import li.cil.oc.api.Driver
 import li.cil.oc.integration.ModProxy
 import li.cil.oc.integration.Mods
@@ -8,6 +9,8 @@ object ModRailcraft extends ModProxy {
   override def getMod = Mods.Railcraft
 
   override def initialize() {
+    FMLInterModComms.sendMessage(Mods.IDs.OpenComputers, "registerWrenchTool", "li.cil.oc.integration.railcraft.EventHandlerRailcraft.useWrench")
+
     Driver.add(new DriverBoilerFirebox)
     Driver.add(new DriverSteamTurbine)
   }
