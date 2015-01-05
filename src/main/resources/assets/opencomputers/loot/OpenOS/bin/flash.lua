27660d6d185fc9610aa8d12a601f69919437a700
Florian Nücke <florian@nuecke.de>
2015-01-05 23:05:40 +0100

    Brought a bit more structure into flash.lua.

--- src/main/resources/assets/opencomputers/loot/OpenOS/bin/flash.lua
+++ src/main/resources/assets/opencomputers/loot/OpenOS/bin/flash.lua
@@ -14,12 +14,11 @@ end
 
 local eeprom = component.eeprom
 
-if options.l then
+local function printRom()
   io.write(eeprom.get())
-  return
 end
 
-if options.r then
+local function readRom()
   fileName = shell.resolve(args[1])
   if not options.q then
     if fs.exists(fileName) then
@@ -31,47 +30,57 @@ if options.r then
     end
     io.write("Reading EEPROM " .. eeprom.address .. ".\n" )
   end
-  file = io.open(fileName, 'wb')
   local bios = eeprom.get()
+  local file = assert(io.open(fileName, "wb"))
   file:write(bios)
   file:close()
-  if not options.q then io.write("All done!\nThe label is '" .. eeprom.getLabel() .. "'.\n") end
-  return
-end
-
-local file = assert(io.open(args[1], "rb"))
-
-if not options.q then
-  io.write("Insert the EEPROM you would like to flash.\n")
-  io.write("When ready to write, type `y` to confirm.\n")
-  repeat
-    local response = io.read()
-  until response and response:lower():sub(1, 1) == "y"
-  io.write("Beginning to flash EEPROM.\n")
-end
-
-if not options.q then
-  io.write("Flashing EEPROM " .. eeprom.address .. ".\n")
-  io.write("Please do NOT power down or restart your computer during this operation!\n")
-end
-
-local bios = file:read("*a")
-file:close()
-
-eeprom.set(bios)
-
-local label = args[2]
-if not options.q and not label then
-  io.write("Enter new label for this EEPROM. Leave input blank to leave the label unchanged.\n")
-  label = io.read()
-end
-if label and #label > 0 then
-  eeprom.setLabel(label)
   if not options.q then
-    io.write("Set label to '" .. eeprom.getLabel() .. "'.\n")
+    io.write("All done!\nThe label is '" .. eeprom.getLabel() .. "'.\n")
   end
 end
 
-if not options.q then
-  io.write("All done! You can remove the EEPROM and re-insert the previous one now.\n")
+local function writeRom()
+  local file = assert(io.open(args[1], "rb"))
+  local bios = file:read("*a")
+  file:close()
+
+  if not options.q then
+    io.write("Insert the EEPROM you would like to flash.\n")
+    io.write("When ready to write, type `y` to confirm.\n")
+    repeat
+      local response = io.read()
+    until response and response:lower():sub(1, 1) == "y"
+    io.write("Beginning to flash EEPROM.\n")
+  end
+
+  if not options.q then
+    io.write("Flashing EEPROM " .. eeprom.address .. ".\n")
+    io.write("Please do NOT power down or restart your computer during this operation!\n")
+  end
+
+  eeprom.set(bios)
+
+  local label = args[2]
+  if not options.q and not label then
+    io.write("Enter new label for this EEPROM. Leave input blank to leave the label unchanged.\n")
+    label = io.read()
+  end
+  if label and #label > 0 then
+    eeprom.setLabel(label)
+    if not options.q then
+      io.write("Set label to '" .. eeprom.getLabel() .. "'.\n")
+    end
+  end
+
+  if not options.q then
+    io.write("All done! You can remove the EEPROM and re-insert the previous one now.\n")
+  end
+end
+
+if options.l then
+  printRom()
+elseif options.r then
+  readRom()
+else
+  writeRom()
 end
