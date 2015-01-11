1ab0698ceb56cef8066234e3da557240120de44f
Florian Nücke <florian@nuecke.de>
2015-01-11 01:57:19 +0100

    Version bumpery.

--- src/main/scala/li/cil/oc/integration/nei/NEIOpenComputersConfig.scala
+++ src/main/scala/li/cil/oc/integration/nei/NEIOpenComputersConfig.scala
@@ -7,14 +7,14 @@ import codechicken.nei.config.OptionToggleButton
 import codechicken.nei.guihook.GuiContainerManager
 import cpw.mods.fml.relauncher.Side
 import cpw.mods.fml.relauncher.SideOnly
+import li.cil.oc.OpenComputers
 import li.cil.oc.integration.util.NEI
-import net.minecraft.item.ItemStack
 
 @SideOnly(Side.CLIENT)
 class NEIOpenComputersConfig extends IConfigureNEI {
-  override def getName = "OpenComputers"
+  override def getName = OpenComputers.Name
 
-  override def getVersion = "1.1.0"
+  override def getVersion = OpenComputers.Version
 
   override def loadConfig() {
     // Non-alphabetic order haunts my OCD, but I want the "Manual" to show up
