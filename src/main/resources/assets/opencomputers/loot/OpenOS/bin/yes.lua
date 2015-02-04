36a2661922298ced02cf08b25138db005b6a3866
Martin Hunek <hunator@users.noreply.github.com>
2015-02-04 17:59:27 +0100

    Update yes.lua

--- src/main/resources/assets/opencomputers/loot/OpenOS/bin/yes.lua
+++ src/main/resources/assets/opencomputers/loot/OpenOS/bin/yes.lua
@@ -3,22 +3,21 @@ local shell = require("shell")
 
 local args, options = shell.parse(...)
 
--- Version option as in GNU coreutils, but in ther case it is "--version"
-if options.V then
+if options.V or options.version then
   io.write("yes v:1.0-2\n")
   io.write("Inspired by functionality of yes from GNU coreutils\n")
   return 0
 end
--- Help option as in GNU coreutils, but in ther case it is "--help"
-if options.h then
+
+if options.h or options.help then
   io.write("Usage: yes [string]...\n")
   io.write("OR:    yes [-V/h]\n")
   io.write("\n")
   io.write("yes prints the command line arguments, or 'y', until is killed.\n")
   io.write("\n")
   io.write("Options:\n")
-  io.write("	-V	Version\n")
-  io.write("	-h	This help\n")
+  io.write("	-V, --version	Version\n")
+  io.write("	-h, --help  	This help\n")
   return 0
 end
 
