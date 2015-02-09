d36661883606c77508ab32e6e3e2c0b7c77efcac
Florian Nücke <florian@nuecke.de>
2015-02-10 00:04:17 +0100

    Fixes creative tab crashing.

--- src/main/scala/li/cil/oc/CreativeTab.scala
+++ src/main/scala/li/cil/oc/CreativeTab.scala
@@ -3,7 +3,11 @@ package li.cil.oc
 import net.minecraft.creativetab.CreativeTabs
 
 object CreativeTab extends CreativeTabs(CreativeTabs.getNextID, "OpenComputers") {
-  override def getTabIconItem = api.Items.get("case1").item()
+  private lazy val stack = api.Items.get("case1").createItemStack(1)
+
+  override def getTabIconItem = stack.getItem
+
+  override def getIconItemDamage = stack.getItemDamage
 
   override def getTranslatedTabLabel = getTabLabel
 }
