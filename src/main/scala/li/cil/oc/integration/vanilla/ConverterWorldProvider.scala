162940191681aa516c11c62bcc824e714ff23467
Ben Blank <ben.blank@gmail.com>
2015-02-10 17:12:03 -0800

    hash dimension ID with world seed to obscure it

--- src/main/scala/li/cil/oc/integration/vanilla/ConverterWorldProvider.scala
+++ src/main/scala/li/cil/oc/integration/vanilla/ConverterWorldProvider.scala
@@ -1,6 +1,9 @@
 package li.cil.oc.integration.vanilla
 
+import java.nio.ByteBuffer
+import java.security.MessageDigest
 import java.util
+import java.util.UUID
 
 import li.cil.oc.api
 import net.minecraft.world
@@ -11,7 +14,12 @@ object ConverterWorldProvider extends api.driver.Converter {
   override def convert(value: AnyRef, output: util.Map[AnyRef, AnyRef]) =
     value match {
       case provider: world.WorldProvider =>
-        output += "id" -> Int.box(provider.dimensionId)
+        val digest = MessageDigest.getInstance("MD5")
+
+        digest.update(ByteBuffer.allocate(8).putLong(provider.getSeed).array)
+        digest.update(ByteBuffer.allocate(4).putInt(provider.dimensionId).array)
+
+        output += "id" -> UUID.nameUUIDFromBytes(digest.digest()).toString
         output += "name" -> provider.getDimensionName
       case _ =>
     }
