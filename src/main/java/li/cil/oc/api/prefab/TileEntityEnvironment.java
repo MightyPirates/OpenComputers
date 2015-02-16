a0b6ff4b6d9ce009518e8e59be7277edbe400455
Florian Nücke <florian@nuecke.de>
2015-02-16 23:31:19 +0100

    This isn't MC1.8... fixed update in tile entity env prefab.

--- src/main/java/li/cil/oc/api/prefab/TileEntityEnvironment.java
+++ src/main/java/li/cil/oc/api/prefab/TileEntityEnvironment.java
@@ -6,7 +6,6 @@ import li.cil.oc.api.network.Message;
 import li.cil.oc.api.network.Node;
 import li.cil.oc.api.network.Visibility;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.server.gui.IUpdatePlayerListBox;
 import net.minecraft.tileentity.TileEntity;
 
 /**
@@ -18,7 +17,7 @@ import net.minecraft.tileentity.TileEntity;
  * network as an index structure to find other nodes connected to them.
  */
 @SuppressWarnings("UnusedDeclaration")
-public abstract class TileEntityEnvironment extends TileEntity implements Environment, IUpdatePlayerListBox {
+public abstract class TileEntityEnvironment extends TileEntity implements Environment {
     /**
      * This must be set in subclasses to the node that is used to represent
      * this tile entity.
@@ -97,7 +96,7 @@ public abstract class TileEntityEnvironment extends TileEntity implements Enviro
     // ----------------------------------------------------------------------- //
 
     @Override
-    public void update() {
+    public void updateEntity() {
         // On the first update, try to add our node to nearby networks. We do
         // this in the update logic, not in validate() because we need to access
         // neighboring tile entities, which isn't possible in validate().
