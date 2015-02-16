bfb1347e8ca188b2cd00900b8385f29838d39dad
Łukasz Magiera <magik6k@users.noreply.github.com>
2015-02-16 20:58:46 +0100

    Done some suggestions in dmesg.lua

--- src/main/resources/assets/opencomputers/loot/OpenOS/bin/dmesg.lua
+++ src/main/resources/assets/opencomputers/loot/OpenOS/bin/dmesg.lua
@@ -1,14 +1,16 @@
 local event = require "event"
 local component = require "component"
+local keyboard = require "keyboard"
 
 local interactive = io.output() == io.stdout
 local color, isPal, evt
 if interactive then
   color, isPal = component.gpu.getForeground()
 end
+io.write("Press 'q' to exit\n")
 pcall(function()
   repeat
-    evt = {event.pull()}
+    evt = table.pack(event.pull())
     if interactive then component.gpu.setForeground(0xCC2200) end
     io.write("[" .. os.date("%T") .. "] ")
     if interactive then component.gpu.setForeground(0x44CC00) end
@@ -16,14 +18,14 @@ pcall(function()
     if interactive then component.gpu.setForeground(0xB0B00F) end
     io.write(tostring(evt[2]) .. string.rep(" ", 37 - #tostring(evt[2])))
     if interactive then component.gpu.setForeground(0xFFFFFF) end
-    if #evt > 2 then
-      for i = 3, #evt do
+    if evt.n > 2 then
+      for i = 3, evt.n do
         io.write("  " .. tostring(evt[i]))
       end
     end
     
     io.write("\n")
-  until evt[1] == "key_down" and evt[3] == 113
+  until evt[1] == "key_down" and evt[4] == keyboard.keys.q
 end)
 if interactive then
   component.gpu.setForeground(color, isPal)
