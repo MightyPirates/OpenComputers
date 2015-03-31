ef86ca01c8d7097cc1d71c4ee1987c81d927bd38
Florian Nücke <florian@nuecke.de>
2015-03-31 13:35:45 +0200

    Added drivers for the separate RF energy APIs, closes #989.

--- src/main/scala/li/cil/oc/integration/cofh/energy/ModCoFHEnergy.scala
+++ src/main/scala/li/cil/oc/integration/cofh/energy/ModCoFHEnergy.scala
@@ -1,20 +1,33 @@
 package li.cil.oc.integration.cofh.energy
 
+import cpw.mods.fml.common.ModAPIManager
 import cpw.mods.fml.common.event.FMLInterModComms
+import cpw.mods.fml.common.versioning.VersionRange
 import li.cil.oc.api.Driver
 import li.cil.oc.integration.ModProxy
 import li.cil.oc.integration.Mods
 import net.minecraftforge.common.MinecraftForge
 
+import scala.collection.convert.WrapAsScala._
+
 object ModCoFHEnergy extends ModProxy {
   override def getMod = Mods.CoFHEnergy
+
+  private val versionsUsingSplitEnergyAPI = VersionRange.createFromVersionSpec("[1.0.0,)")
 
   override def initialize() {
     FMLInterModComms.sendMessage(Mods.IDs.OpenComputers, "registerToolDurabilityProvider", "li.cil.oc.integration.cofh.energy.EventHandlerRedstoneFlux.getDurability")
 
     MinecraftForge.EVENT_BUS.register(EventHandlerRedstoneFlux)
 
-    Driver.add(new DriverEnergyHandler)
+    val apiVersion = ModAPIManager.INSTANCE.getAPIList.find(_.getModId == Mods.IDs.CoFHEnergy).map(_.getProcessedVersion)
+    if (apiVersion.exists(versionsUsingSplitEnergyAPI.containsVersion)) {
+      Driver.add(new DriverEnergyProvider)
+      Driver.add(new DriverEnergyReceiver)
+    }
+    else {
+      Driver.add(new DriverEnergyHandler)
+    }
 
     Driver.add(new ConverterEnergyContainerItem)
   }
