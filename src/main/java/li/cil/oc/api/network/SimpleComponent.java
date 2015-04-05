a41f3a89ba98ecc0b5b8477870444d4638e1ac69
Florian Nücke <florian@nuecke.de>
2015-04-05 15:44:18 +0200

    Bump Forge version, fixed link in Javadoc.

--- src/main/java/li/cil/oc/api/network/SimpleComponent.java
+++ src/main/java/li/cil/oc/api/network/SimpleComponent.java
@@ -9,8 +9,8 @@ package li.cil.oc.api.network;
  * couple of methods to the programs running computers.
  * <p/>
  * This is an interface instead of an annotation, to allow stripping via the
- * ever so handy {@link cpw.mods.fml.common.Optional} annotation, meaning there
- * will be no strong dependency on OpenComputers.
+ * ever so handy {@link net.minecraftforge.fml.common.Optional} annotation,
+ * meaning there will be no strong dependency on OpenComputers.
  * <p/>
  * Classes implementing this interface will be expanded with the methods
  * required for them to function as native block components (say, like the
