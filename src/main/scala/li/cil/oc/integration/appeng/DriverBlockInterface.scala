38b6f4b9399d179e1ff3acdc07e7ae4fa4acf791
Florian Nücke <florian@nuecke.de>
2015-04-05 17:53:36 +0200

    Added back old generic export bus methods to avoid breaking things, marked them deprecated. Gave the AE2 integration a bit of a refactor, pulling out common functionality for parts. Fixed a few of the callback docs. General cleanup.

--- src/main/scala/li/cil/oc/integration/appeng/DriverBlockInterface.scala
+++ src/main/scala/li/cil/oc/integration/appeng/DriverBlockInterface.scala
@@ -1,78 +1,62 @@
 package li.cil.oc.integration.appeng
 
-import appeng.api.AEApi
 import appeng.tile.misc.TileInterface
-import cpw.mods.fml.common.versioning.VersionRange
-import cpw.mods.fml.common.Loader
-import li.cil.oc.api.driver.NamedBlock
 import li.cil.oc.api.driver.EnvironmentAware
+import li.cil.oc.api.driver.NamedBlock
 import li.cil.oc.api.internal.Database
-import li.cil.oc.api.prefab.DriverTileEntity
+import li.cil.oc.api.machine.Arguments
 import li.cil.oc.api.machine.Callback
 import li.cil.oc.api.machine.Context
-import li.cil.oc.api.machine.Arguments
-import li.cil.oc.api.network.ManagedEnvironment
 import li.cil.oc.api.network.Component
-import li.cil.oc.integration.Mods
+import li.cil.oc.api.network.ManagedEnvironment
+import li.cil.oc.api.prefab.DriverTileEntity
 import li.cil.oc.integration.ManagedTileEntityEnvironment
 import li.cil.oc.util.ExtendedArguments._
 import li.cil.oc.util.ResultWrapper._
-import net.minecraft.block.Block
 import net.minecraft.item.ItemStack
 import net.minecraft.world.World
-import net.minecraftforge.common.util.ForgeDirection
-
-
 
 object DriverBlockInterface extends DriverTileEntity with EnvironmentAware {
   def getTileEntityClass: Class[_] = classOf[TileInterface]
-  
+
   def createEnvironment(world: World, x: Int, y: Int, z: Int): ManagedEnvironment =
     new Environment(world.getTileEntity(x, y, z).asInstanceOf[TileInterface])
-  
-  override def providedEnvironment(stack: ItemStack) = 
-    if (stack != null &&
-        AEApi.instance != null &&
-      AEApi.instance.blocks != null &&
-      AEApi.instance.blocks.blockInterface != null &&
-      Block.getBlockFromItem(stack.getItem) == AEApi.instance().blocks().blockInterface.block) classOf[Environment]
+
+  override def providedEnvironment(stack: ItemStack) =
+    if (AEUtil.isBlockInterface(stack)) classOf[Environment]
     else null
-  
+
   class Environment(tileEntity: TileInterface) extends ManagedTileEntityEnvironment[TileInterface](tileEntity, "me_interface") with NamedBlock {
     override def preferredName = "me_interface"
 
     override def priority = 1
-      
+
     @Callback(doc = "function([slot:number]):table -- Get the configuration of the interface.")
     def getInterfaceConfiguration(context: Context, args: Arguments): Array[AnyRef] = {
       val config = tileEntity.getInventoryByName("config")
-      val slot = args.optSlot(config, 2, 0)
+      val slot = args.optSlot(config, 0, 0)
       val stack = config.getStackInSlot(slot)
       result(stack)
     }
-    
-    @Callback(doc = "function([slot:number][, database:address, entry:number, size:number]):boolean -- Configure the interface pointing in the specified direction..")
-    def setInterfaceConfiguration(context: Context, args: Arguments): Array[AnyRef] = {
 
+    @Callback(doc = "function([slot:number][, database:address, entry:number[, size:number]]):boolean -- Configure the interface.")
+    def setInterfaceConfiguration(context: Context, args: Arguments): Array[AnyRef] = {
       val config = tileEntity.getInventoryByName("config")
-      val slot = if (args.count > 3 || args.count < 3) args.optSlot(config, 0, 0) else 0
+      val slot = if (args.isString(0)) 0 else args.optSlot(config, 0, 0)
       val stack = if (args.count > 1) {
         val (address, entry, size) =
-          if (args.count > 3) (args.checkString(1), args.checkInteger(2), args.checkInteger(3))
-          else (args.checkString(0), args.checkInteger(1), args.checkInteger(2))
+          if (args.isString(0)) (args.checkString(0), args.checkInteger(1), args.optInteger(2, 1))
+          else (args.checkString(1), args.checkInteger(2), args.optInteger(3, 1))
+
         node.network.node(address) match {
           case component: Component => component.host match {
-            case database: Database => {
-              val s = database.getStackInSlot(entry - 1)
-              if (s == null)
-                null
-              else{
-                if (size <= 0)
-                  null
-                s.stackSize = Math.min(size, s.getMaxStackSize)
-                s
+            case database: Database =>
+              val dbStack = database.getStackInSlot(entry - 1)
+              if (dbStack == null || size < 1) null
+              else {
+                dbStack.stackSize = math.min(size, dbStack.getMaxStackSize)
+                dbStack
               }
-            }
             case _ => throw new IllegalArgumentException("not a database")
           }
           case _ => throw new IllegalArgumentException("no such component")
@@ -84,4 +68,5 @@ object DriverBlockInterface extends DriverTileEntity with EnvironmentAware {
       result(true)
     }
   }
+
 }
