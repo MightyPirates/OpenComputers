88c30ba96fc1869c5a188ff4884b9eac57434d98
dgelessus <dgelessus@users.noreply.github.com>
2015-04-19 20:23:19 +0200

    Basic self-update program for OpenOS
    * --ignore option for install to specify folders in which files should
    not be overwritten
    * update command that runs install in a non-destructive way

--- src/main/resources/assets/opencomputers/loot/OpenOS/bin/install.lua
+++ src/main/resources/assets/opencomputers/loot/OpenOS/bin/install.lua
@@ -1,8 +1,54 @@
 local component = require("component")
 local computer = require("computer")
 local event = require("event")
-local unicode = require("unicode")
+local fs = require("filesystem")
 local shell = require("shell")
+local unicode = require("unicode")
+
+-- Modified version of recurse from /bin/cp.lua
+local function rcopy(fromPath, toPath, ignore)
+  ignore = ignore or {}
+  os.sleep(0) -- allow interrupting
+  if fs.isDirectory(fromPath) then
+    if fs.canonical(fromPath) == fs.canonical(fs.path(toPath)) then
+      return nil, "cannot copy a directory, `" .. fromPath .. "', into itself, `" .. toPath .. "'\n"
+    end
+    if fs.exists(toPath) and not fs.isDirectory(toPath) then
+      -- my real cp always does this, even with -f, -n or -i.
+      return nil, "cannot overwrite non-directory `" .. toPath .. "' with directory `" .. fromPath .. "'"
+    end
+    print(fromPath .. " -> " .. toPath)
+    fs.makeDirectory(toPath)
+    for file in fs.list(fromPath) do
+      local result, reason = rcopy(fs.concat(fromPath, file), fs.concat(toPath, file), ignore)
+      if not result then
+        return nil, reason
+      end
+    end
+    return true
+  else
+    if fs.exists(toPath) then
+      if fs.canonical(fromPath) == fs.canonical(toPath) then
+        return nil, "`" .. fromPath .. "' and `" .. toPath .. "' are the same file"
+      end
+      if fs.isDirectory(toPath) then
+        return nil, "cannot overwrite directory `" .. toPath .. "' with non-directory"
+      else
+        for n, part in ipairs(ignore) do
+          print("match ^" .. part .. " on " .. fs.canonical(toPath))
+          if fs.canonical(toPath):match("^" .. part) then
+            print("Skipping " .. fromPath .. " -> " .. toPath)
+            return true -- file is in ignored directory
+          end
+        end
+        -- else: default to overwriting
+      end
+      fs.remove(toPath)
+    end
+    print(fromPath .. " -> " .. toPath)
+    return fs.copy(fromPath, toPath)
+  end
+end
 
 local args, options = shell.parse(...)
 
@@ -47,12 +93,18 @@ end
 candidates = nil
 
 local name = options.name or "OpenOS"
-print("Installing " .. name .." to device " .. (choice.getLabel() or choice.address))
+print("Installing " .. name .. " to device " .. (choice.getLabel() or choice.address))
 os.sleep(0.25)
 local origin = options.from and options.from:sub(1,3) or computer.getBootAddress():sub(1, 3)
 local fromDir = options.fromDir or "/"
 local mnt = choice.address:sub(1, 3)
-local result, reason = os.execute("/bin/cp -vr /mnt/" .. origin .. fromDir .. "* /mnt/" .. mnt .. "/")
+
+local ignore = {}
+for part in string.gmatch(options.ignore, "[^:]+") do
+  table.insert(ignore, fs.canonical("/mnt/" .. mnt .. "/" .. part))
+end
+
+local result, reason = rcopy("/mnt/" .. origin .. fromDir, "/mnt/" .. mnt .. "/", ignore)
 if not result then
   error(reason, 0)
 end
---
+++ src/main/resources/assets/opencomputers/loot/OpenOS/bin/update.lua
@@ -0,0 +1,20 @@
+local component = require("component")
+local computer = require("computer")
+local args, options = shell.parse(...)
+local from = "--from="
+
+if not options.from then
+  for addr in component.list("filesystem") do
+    if addr ~= computer.getBootAddress() and component.proxy(addr).getLabel() == "openos" then
+      from = from .. addr
+      break
+    end
+  end
+  
+  if from == "--from=" then
+    print("Could not find an OpenOS boot device to update from.")
+    return
+  end
+end
+
+return os.execute("install " .. from .. " --ignore=/etc --noboot --nolabelset " .. table.concat(table.pack(...), " "))
