d0062067d1a8ba201dd963cdaed0bf9f2b91c19a
Florian Nücke <florian@nuecke.de>
2015-04-19 17:48:39 +0200

    Should fix #1085 for good... probably.

--- src/main/scala/li/cil/oc/client/gui/ServerRack.scala
+++ src/main/scala/li/cil/oc/client/gui/ServerRack.scala
@@ -70,13 +70,12 @@ class ServerRack(playerInventory: InventoryPlayer, val rack: tileentity.ServerRa
   }
 
   override def drawScreen(mouseX: Int, mouseY: Int, dt: Float) {
-    for (i <- 0 to 3 if powerButtons(i) != null) {
+    if (powerButtons(0) == null) return // This should be impossible, but was reported to happen...
+    for (i <- 0 to 3) {
       powerButtons(i).toggled = rack.isRunning(i)
       sideButtons(i).displayString = sideName(i)
     }
-    if (switchButton != null) {
-      switchButton.displayString = if (rack.internalSwitch) Localization.ServerRack.SwitchInternal else Localization.ServerRack.SwitchExternal
-    }
+    switchButton.displayString = if (rack.internalSwitch) Localization.ServerRack.SwitchInternal else Localization.ServerRack.SwitchExternal
     super.drawScreen(mouseX, mouseY, dt)
   }
 
