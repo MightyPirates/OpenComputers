15cc9a7146cf3b0fa9ccac31703abb4587e1d284
Florian Nücke <florian@nuecke.de>
2015-04-29 16:18:21 +0200

    Caching redstone devices for bluepower.

--- src/main/scala/li/cil/oc/integration/bluepower/RedstoneProvider.scala
+++ src/main/scala/li/cil/oc/integration/bluepower/RedstoneProvider.scala
@@ -9,21 +9,27 @@ import li.cil.oc.common.tileentity.traits.RedstoneAware
 import net.minecraft.world.World
 import net.minecraftforge.common.util.ForgeDirection
 
+import scala.collection.mutable
+
 object RedstoneProvider extends IRedstoneProvider {
+  val redstoneDevices = mutable.WeakHashMap.empty[RedstoneAware, RedstoneDevice]
+
+  val bundledRedstoneDevices = mutable.WeakHashMap.empty[BundledRedstoneAware, BundledRedstoneDevice]
+
   def init(): Unit = {
     BPApi.getInstance.getRedstoneApi.registerRedstoneProvider(this)
   }
 
   override def getRedstoneDeviceAt(world: World, x: Int, y: Int, z: Int, side: ForgeDirection, face: ForgeDirection): IRedstoneDevice = {
     world.getTileEntity(x, y, z) match {
-      case tileEntity: RedstoneAware => new RedstoneDevice(tileEntity)
+      case tileEntity: RedstoneAware => redstoneDevices.getOrElseUpdate(tileEntity, new RedstoneDevice(tileEntity))
       case _ => null
     }
   }
 
   override def getBundledDeviceAt(world: World, x: Int, y: Int, z: Int, side: ForgeDirection, face: ForgeDirection): IBundledDevice = {
     world.getTileEntity(x, y, z) match {
-      case tileEntity: BundledRedstoneAware => new BundledRedstoneDevice(tileEntity)
+      case tileEntity: BundledRedstoneAware => bundledRedstoneDevices.getOrElseUpdate(tileEntity, new BundledRedstoneDevice(tileEntity))
       case _ => null
     }
   }
