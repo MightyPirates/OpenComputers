adf68933ab23bd04307472ff5a651056f6366068
Florian Nücke <florian@nuecke.de>
2015-06-07 12:03:11 +0200

    Didn't need you after all.

--- src/main/scala/li/cil/oc/common/recipe/NBTIgnoringShapedRecipe.scala
+++
@@ -1,8 +0,0 @@
-package li.cil.oc.common.recipe
-
-import net.minecraft.item.ItemStack
-import net.minecraftforge.oredict.ShapedOreRecipe
-
-class NBTIgnoringShapedRecipe(result: ItemStack, recipe: AnyRef*) extends ShapedOreRecipe(result, recipe: _*) {
-
-}
