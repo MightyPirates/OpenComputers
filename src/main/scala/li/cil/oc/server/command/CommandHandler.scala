5c8572ba379ce0bc0ba2be0d47eab0da32aebb67
Florian Nücke <florian@nuecke.de>
2015-07-11 22:48:41 +0200

    Added command to spawn a creative computer with the basic stuff built-in. Closes #1289.

--- src/main/scala/li/cil/oc/server/command/CommandHandler.scala
+++ src/main/scala/li/cil/oc/server/command/CommandHandler.scala
@@ -4,7 +4,8 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent
 
 object CommandHandler {
   def register(e: FMLServerStartingEvent) {
-    e.registerServerCommand(WirelessRenderingCommand)
     e.registerServerCommand(NonDisassemblyAgreementCommand)
+    e.registerServerCommand(WirelessRenderingCommand)
+    e.registerServerCommand(SpawnComputerCommand)
   }
 }
