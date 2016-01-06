851d869c83aa9cc901c84144df92e10b495855c2
Florian Nücke <florian@nuecke.de>
2016-01-06 13:13:38 +0100

    Should fix #1589.

--- src/main/scala/li/cil/oc/integration/mfr/ConverterSafariNet.scala
+++ src/main/scala/li/cil/oc/integration/mfr/ConverterSafariNet.scala
@@ -11,7 +11,7 @@ import scala.collection.convert.WrapAsScala._
 
 object ConverterSafariNet extends Converter {
   override def convert(value: scala.Any, output: util.Map[AnyRef, AnyRef]): Unit = value match {
-    case stack: ItemStack if stack.getItem.isInstanceOf[ItemSafariNet] =>
+    case stack: ItemStack if stack.getItem.isInstanceOf[ItemSafariNet] && stack.hasTagCompound =>
       val nbt = stack.getTagCompound
       if (nbt.hasKey("id", NBT.TAG_STRING) && !nbt.getBoolean("hide")) {
         output += "entity" -> nbt.getString("id")
