44e771cb7587a851c32438be7163efcd97bbb931
Florian Nücke <florian@nuecke.de>
2016-02-27 11:33:12 +0100

    Limit max number of items crafted in one command to one stack, closes #1651. Prevents semi-infinite loops for identity-crafting (i.e. input == output).

--- src/main/scala/li/cil/oc/server/component/UpgradeCrafting.scala
+++ src/main/scala/li/cil/oc/server/component/UpgradeCrafting.scala
@@ -27,7 +27,7 @@ class UpgradeCrafting(val host: EnvironmentHost with internal.Robot) extends pre
 
   @Callback(doc = """function([count:number]):number -- Tries to craft the specified number of items in the top left area of the inventory.""")
   def craft(context: Context, args: Arguments): Array[AnyRef] = {
-    val count = args.optInteger(0, Int.MaxValue)
+    val count = args.optInteger(0, 64) max 0 min 64
     result(CraftingInventory.craft(count): _*)
   }
 
