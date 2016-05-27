ba59e9493f9dd3f953d269491508b38938cc7f9e
Florian Nücke <florian@nuecke.de>
2016-05-27 15:07:28 +0200

    JEI integration for loot disk recrafting (more or less :P).

--- src/main/scala/li/cil/oc/integration/jei/ModPluginOpenComputers.scala
+++ src/main/scala/li/cil/oc/integration/jei/ModPluginOpenComputers.scala
@@ -1,5 +1,11 @@
 package li.cil.oc.integration.jei
 
+import java.util
+
+import li.cil.oc.Constants
+import li.cil.oc.api
+import li.cil.oc.common.Loot
+import li.cil.oc.common.recipe.LootDiskCyclingRecipe
 import li.cil.oc.integration.util.ItemBlacklist
 import mezz.jei.api.IItemRegistry
 import mezz.jei.api.IJeiHelpers
@@ -8,6 +14,14 @@ import mezz.jei.api.IModPlugin
 import mezz.jei.api.IModRegistry
 import mezz.jei.api.IRecipeRegistry
 import mezz.jei.api.JEIPlugin
+import mezz.jei.api.recipe.BlankRecipeWrapper
+import mezz.jei.api.recipe.IRecipeHandler
+import mezz.jei.api.recipe.IRecipeWrapper
+import mezz.jei.api.recipe.VanillaRecipeCategoryUid
+import mezz.jei.api.recipe.wrapper.ICraftingRecipeWrapper
+import net.minecraft.item.ItemStack
+
+import scala.collection.convert.WrapAsJava._
 
 @JEIPlugin
 class ModPluginOpenComputers extends IModPlugin {
@@ -19,7 +33,7 @@ class ModPluginOpenComputers extends IModPlugin {
   }
 
   override def register(registry: IModRegistry): Unit = {
-    registry.addRecipeHandlers()
+    registry.addRecipeHandlers(LootDiskCyclingRecipeHandler)
   }
 
   override def onRecipeRegistryAvailable(recipeRegistry: IRecipeRegistry): Unit = {
@@ -27,4 +41,21 @@ class ModPluginOpenComputers extends IModPlugin {
 
   override def onRuntimeAvailable(jeiRuntime: IJeiRuntime): Unit = {
   }
+
+  object LootDiskCyclingRecipeHandler extends IRecipeHandler[LootDiskCyclingRecipe] {
+    override def getRecipeClass: Class[LootDiskCyclingRecipe] = classOf[LootDiskCyclingRecipe]
+
+    override def getRecipeCategoryUid: String = VanillaRecipeCategoryUid.CRAFTING
+
+    override def getRecipeWrapper(recipe: LootDiskCyclingRecipe): IRecipeWrapper = new LootDiskCyclingRecipeWrapper(recipe)
+
+    override def isRecipeValid(recipe: LootDiskCyclingRecipe): Boolean = true
+  }
+
+  class LootDiskCyclingRecipeWrapper(val recipe: LootDiskCyclingRecipe) extends BlankRecipeWrapper with ICraftingRecipeWrapper {
+    override def getInputs: util.List[_] = List(seqAsJavaList(Loot.worldDisks.map(_._1)), api.Items.get(Constants.ItemName.Wrench).createItemStack(1))
+
+    override def getOutputs: util.List[ItemStack] = Loot.worldDisks.map(_._1).toList
+  }
+
 }
