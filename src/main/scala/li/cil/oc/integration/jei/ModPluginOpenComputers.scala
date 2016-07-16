b2e06800fd19055a3b069f9d9e50ec3b4a774fcb
Vexatos <Stuarzt@gmx.de>
2016-07-16 14:09:13 +0200

    Partially added item highlighting for JEI.

--- src/main/scala/li/cil/oc/integration/jei/ModPluginOpenComputers.scala
+++ src/main/scala/li/cil/oc/integration/jei/ModPluginOpenComputers.scala
@@ -2,6 +2,7 @@ package li.cil.oc.integration.jei
 
 import li.cil.oc.Settings
 import li.cil.oc.integration.util.ItemBlacklist
+import li.cil.oc.integration.util.ItemSearch
 import mezz.jei.api.IItemRegistry
 import mezz.jei.api.IJeiHelpers
 import mezz.jei.api.IJeiRuntime
@@ -9,17 +10,20 @@ import mezz.jei.api.IModPlugin
 import mezz.jei.api.IModRegistry
 import mezz.jei.api.IRecipeRegistry
 import mezz.jei.api.JEIPlugin
+import net.minecraft.client.gui.inventory.GuiContainer
+import net.minecraft.item.ItemStack
 
 @JEIPlugin
 class ModPluginOpenComputers extends IModPlugin {
-  override def onJeiHelpersAvailable(jeiHelpers: IJeiHelpers): Unit = {
+
+  override def onJeiHelpersAvailable(jeiHelpers: IJeiHelpers) {
     ItemBlacklist.consumers += jeiHelpers.getItemBlacklist.addItemToBlacklist
   }
 
-  override def onItemRegistryAvailable(itemRegistry: IItemRegistry): Unit = {
+  override def onItemRegistryAvailable(itemRegistry: IItemRegistry) {
   }
 
-  override def register(registry: IModRegistry): Unit = {
+  override def register(registry: IModRegistry) {
     if (Settings.get.lootRecrafting) {
       registry.addRecipeHandlers(LootDiskCyclingRecipeHandler)
     }
@@ -36,9 +40,17 @@ class ModPluginOpenComputers extends IModPlugin {
     registry.addRecipes(CallbackDocHandler.getRecipes(registry))
   }
 
-  override def onRecipeRegistryAvailable(recipeRegistry: IRecipeRegistry): Unit = {
+  override def onRecipeRegistryAvailable(recipeRegistry: IRecipeRegistry) {
   }
 
-  override def onRuntimeAvailable(jeiRuntime: IJeiRuntime): Unit = {
+  var stackUnderMouse: (GuiContainer, Int, Int) => Option[ItemStack] = null
+
+  override def onRuntimeAvailable(jeiRuntime: IJeiRuntime) {
+    if (stackUnderMouse == null) {
+      ItemSearch.stackFocusing += ((container, mouseX, mouseY) => stackUnderMouse(container, mouseX, mouseY))
+    }
+    stackUnderMouse = (container, mouseX, mouseY) => Option(jeiRuntime.getItemListOverlay.getStackUnderMouse)
+
+    // TODO check for focused input
   }
 }
