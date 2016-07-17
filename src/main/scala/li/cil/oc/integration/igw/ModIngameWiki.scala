6a1acdf0be3fca50e79de0c0264540498ae02163
Vexatos <Stuarzt@gmx.de>
2016-07-17 16:46:14 +0200

    Re-added support for IGW-Mod.

--- src/main/scala/li/cil/oc/integration/igw/ModIngameWiki.scala
+++ src/main/scala/li/cil/oc/integration/igw/ModIngameWiki.scala
@@ -1,9 +1,7 @@
 package li.cil.oc.integration.igw
 
-import li.cil.oc.api.Driver
 import li.cil.oc.integration.ModProxy
 import li.cil.oc.integration.Mods
-import net.minecraftforge.common.MinecraftForge
 
 object ModIngameWiki extends ModProxy {
   override def getMod = Mods.IngameWiki
