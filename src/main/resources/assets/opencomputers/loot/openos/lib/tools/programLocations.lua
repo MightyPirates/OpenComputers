e5d2b0051fd2210acc82688735d03146241b6dfb
payonel <payonel@hotmail.com>
2016-10-26 23:37:34 -0700

    simple error reporting and message to craft install disk if user tries to use program known to computer.getProgramLocations()

---
+++ src/main/resources/assets/opencomputers/loot/openos/lib/tools/programLocations.lua
@@ -0,0 +1,23 @@
+local computer = require("computer")
+local lib = {}
+
+function lib.locate(path)
+  for _,lookup in ipairs(computer.getProgramLocations()) do
+    if lookup[1] == path then
+      return lookup[2]
+    end
+  end
+end
+
+function lib.reportNotFound(path, reason)
+  local loot = lib.locate(path)
+  if loot then
+    io.stderr:write("The program '" .. path .. "' is currently not installed.  To install it:\n" ..
+      "1. Craft the '" .. loot .. "' floppy disk and insert it into this computer.\n" ..
+      "2. Run `install " .. loot  .. "`")
+  else
+    io.stderr:write(path .. ": " .. reason .. "\n")
+  end
+end
+
+return lib
