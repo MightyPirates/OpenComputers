43fe39edd13a7a5f8bae7fdd3f9f16329f140ea3
Florian Nücke <florian@nuecke.de>
2017-02-25 16:11:02 +0100

    More cleanup.

--- src/main/scala/li/cil/oc/integration/Mods.scala
+++ src/main/scala/li/cil/oc/integration/Mods.scala
@@ -18,77 +18,13 @@ object Mods {
   // ----------------------------------------------------------------------- //
 
   def All = knownMods.clone()
-
-  val AgriCraft = new SimpleMod(IDs.AgriCraft, version = "@[1.4.0,)")
-  val AppliedEnergistics2 = new SimpleMod(IDs.AppliedEnergistics2, version = "@[rv1,)", providesPower = true)
-  val BattleGear2 = new SimpleMod(IDs.BattleGear2)
-  val BetterRecords = new SimpleMod(IDs.BetterRecords)
   val BloodMagic = new SimpleMod(IDs.BloodMagic)
-  val BluePower = new SimpleMod(IDs.BluePower, version = "@[0.2.928,)")
-  val BuildCraft = new SimpleMod(IDs.BuildCraft)
-  val BuildCraftLibrary = new SimpleMod(IDs.BuildCraftLibrary, version = "@[7.0.9,)")
-  val BuildCraftRecipes = new SimpleMod(IDs.BuildCraftRecipes)
-  val BuildCraftTiles = new SimpleMod(IDs.BuildCraftTiles)
-  val BuildCraftTools = new SimpleMod(IDs.BuildCraftTools)
-  val BuildCraftTransport = new SimpleMod(IDs.BuildCraftTransport)
-  val CoFHEnergy = new SimpleMod(IDs.CoFHEnergy, providesPower = true)
-  val CoFHItem = new SimpleMod(IDs.CoFHItem)
-  val CoFHTileEntity = new SimpleMod(IDs.CoFHTileEntity)
-  val CoFHTransport = new SimpleMod(IDs.CoFHTransport)
-  val ColoredLights = new SimpleMod(IDs.ColoredLights)
-  val ComputerCraft = new SimpleMod(IDs.ComputerCraft, version = "@[1.73,)")
-  val CraftingCosts = new SimpleMod(IDs.CraftingCosts)
-  val DeepStorageUnit = new ClassBasedMod(IDs.DeepStorageUnit, "powercrystals.minefactoryreloaded.api.IDeepStorageUnit")()
-  val ElectricalAge = new SimpleMod(IDs.ElectricalAge, providesPower = true)
-  val EnderIO = new SimpleMod(IDs.EnderIO, version = "@[1.10.2-3.0.1.132,)")
-  val EnderStorage = new SimpleMod(IDs.EnderStorage)
-  val ExtraCells = new SimpleMod(IDs.ExtraCells, version = "@[2.2.73,)")
-  val Factorization = new SimpleMod(IDs.Factorization, providesPower = true)
   val Forestry = new SimpleMod(IDs.Forestry, version = "@[5.2,)")
-  val ForgeMultipart = new SimpleMod(IDs.ForgeMultipart)
-  val Galacticraft = new SimpleMod(IDs.Galacticraft, providesPower = true)
-  val GregTech = new ClassBasedMod(IDs.GregTech, "gregtech.api.GregTech_API")()
-  val IndustrialCraft2 = new SimpleMod(IDs.IndustrialCraft2, providesPower = true)
-  val IndustrialCraft2Classic = new SimpleMod(IDs.IndustrialCraft2Classic, providesPower = true)
-  val IngameWiki = new SimpleMod(IDs.IngameWiki)
   val JustEnoughItems = new SimpleMod(IDs.JustEnoughItems)
   val MCMultiPart = new SimpleMod(IDs.MCMultiPart, version = "@[1.0.9,)")
-  val Mekanism = new SimpleMod(IDs.Mekanism, providesPower = true)
-  val MekanismGas = new SimpleMod(IDs.MekanismGas)
   val Minecraft = new SimpleMod(IDs.Minecraft)
-  val MineFactoryReloaded = new SimpleMod(IDs.MineFactoryReloaded)
-  val Mystcraft = new SimpleMod(IDs.Mystcraft)
-  val NotEnoughItems = new SimpleMod(IDs.NotEnoughItems)
-  val NotEnoughKeys = new SimpleMod(IDs.NotEnoughKeys)
   val OpenComputers = new SimpleMod(IDs.OpenComputers)
-  val PortalGun = new SimpleMod(IDs.PortalGun)
-  val PowerAdvantage = new SimpleMod(IDs.PowerAdvantage, version = "@[1.2.0,)", providesPower = true)
-  val ProjectRedCore = new SimpleMod(IDs.ProjectRedCore)
-  val ProjectRedTransmission = new SimpleMod(IDs.ProjectRedTransmission)
-  val Railcraft = new SimpleMod(IDs.Railcraft)
-  val RedLogic = new SimpleMod(IDs.RedLogic)
-  val RotaryCraft = new SimpleMod(IDs.RotaryCraft)
-  val StargateTech2 = new ModBase {
-    def id = IDs.StargateTech2
-
-    private lazy val isModAvailable_ = Loader.isModLoaded(IDs.StargateTech2) && {
-      val mod = Loader.instance.getIndexedModList.get(IDs.StargateTech2)
-      mod.getVersion.startsWith("0.7.")
-    }
-
-    override def isModAvailable: Boolean = isModAvailable_
-  }
-  val Tesla = new SimpleMod(IDs.Tesla, providesPower = true)
-  val Thaumcraft = new SimpleMod(IDs.Thaumcraft)
-  val ThaumicEnergistics = new SimpleMod(IDs.ThaumicEnergistics)
-  val ThermalExpansion = new SimpleMod(IDs.ThermalExpansion, providesPower = true)
-  val TinkersConstruct = new SimpleMod(IDs.TinkersConstruct)
   val TIS3D = new SimpleMod(IDs.TIS3D, version = "@[0.9,)")
-  val TMechWorks = new SimpleMod(IDs.TMechWorks)
-  val VersionChecker = new SimpleMod(IDs.VersionChecker)
-  val Waila = new SimpleMod(IDs.Waila)
-  val WirelessRedstoneCBE = new SimpleMod(IDs.WirelessRedstoneCBE)
-  val WirelessRedstoneSVE = new SimpleMod(IDs.WirelessRedstoneSV)
 
   // ----------------------------------------------------------------------- //
 
@@ -124,68 +60,13 @@ object Mods {
   // ----------------------------------------------------------------------- //
 
   object IDs {
-    final val AgriCraft = "AgriCraft"
-    final val AppliedEnergistics2 = "appliedenergistics2"
-    final val BattleGear2 = "battlegear2"
-    final val BetterRecords = "betterrecords"
     final val BloodMagic = "AWWayofTime"
-    final val BluePower = "bluepowerAPI"
-    final val BuildCraft = "BuildCraft|Core"
-    final val BuildCraftLibrary = "BuildCraftAPI|library"
-    final val BuildCraftRecipes = "BuildCraftAPI|recipes"
-    final val BuildCraftTiles = "BuildCraftAPI|tiles"
-    final val BuildCraftTools = "BuildCraftAPI|tools"
-    final val BuildCraftTransport = "BuildCraftAPI|transport"
-    final val CoFHEnergy = "CoFHAPI|energy"
-    final val CoFHItem = "CoFHAPI|item"
-    final val CoFHTileEntity = "CoFHAPI|tileentity"
-    final val CoFHTransport = "CoFHAPI|transport"
-    final val ColoredLights = "easycoloredlights"
-    final val ComputerCraft = "ComputerCraft"
-    final val CraftingCosts = "CraftingCosts"
-    final val ElectricalAge = "Eln"
-    final val EnderIO = "EnderIO"
-    final val EnderStorage = "EnderStorage"
-    final val ExtraCells = "extracells"
-    final val Factorization = "factorization"
     final val Forestry = "forestry"
-    final val ForgeMultipart = "ForgeMultipart"
-    final val DeepStorageUnit = "MineFactoryReloaded|DeepStorageUnit"
-    // Doesn't really exist.
-    final val Galacticraft = "Galacticraft API"
-    final val GregTech = "gregtech"
-    final val IndustrialCraft2 = "IC2"
-    final val IndustrialCraft2Classic = "IC2-Classic"
-    final val IngameWiki = "IGWMod"
     final val JustEnoughItems = "JEI"
     final val MCMultiPart = "mcmultipart"
-    final val Mekanism = "Mekanism"
-    final val MekanismGas = "MekanismAPI|gas"
     final val Minecraft = "Minecraft"
-    final val MineFactoryReloaded = "MineFactoryReloaded"
-    final val Mystcraft = "Mystcraft"
-    final val NotEnoughItems = "NotEnoughItems"
-    final val NotEnoughKeys = "notenoughkeys"
     final val OpenComputers = "OpenComputers"
-    final val PortalGun = "PortalGun"
-    final val PowerAdvantage = "poweradvantage"
-    final val ProjectRedCore = "ProjRed|Core"
-    final val ProjectRedTransmission = "ProjRed|Transmission"
-    final val Railcraft = "Railcraft"
-    final val RedLogic = "RedLogic"
-    final val RotaryCraft = "RotaryCraft"
-    final val StargateTech2 = "StargateTech2"
-    final val Tesla = "tesla"
-    final val Thaumcraft = "Thaumcraft"
-    final val ThaumicEnergistics = "thaumicenergistics"
-    final val ThermalExpansion = "ThermalExpansion"
-    final val TinkersConstruct = "TConstruct"
     final val TIS3D = "tis3d"
-    final val TMechWorks = "TMechworks"
-    final val VersionChecker = "VersionChecker"
-    final val Waila = "Waila"
-    final val WirelessRedstoneCBE = "WR-CBE|Core"
-    final val WirelessRedstoneSV = "WirelessRedstone"
   }
 
   // ----------------------------------------------------------------------- //
