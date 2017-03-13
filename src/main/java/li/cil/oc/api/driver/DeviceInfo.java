c548be5fde6eb974c5f49761a3054fe790135d12
Brisingr Aerowing <ztgreve@live.com>
2017-03-13 14:10:24 -0400

    Performed Requested Changes

--- src/main/java/li/cil/oc/api/driver/DeviceInfo.java
+++ src/main/java/li/cil/oc/api/driver/DeviceInfo.java
@@ -69,7 +69,8 @@ public interface DeviceInfo {
         public static final String Size = "size"; // actual size of the device, e.g. actual usable space on a disk
         public static final String Clock = "clock"; // bus clock (in Hz) of the device, e.g. call speed(s) of a component
         public static final String Width = "width"; // address width of the device, in the broadest sense
-
+		public static final String MaxParts = "maxparts"; // Maximum number of parts for, e.g. a network packet.
+		
         private DeviceAttribute() {
         }
     }
