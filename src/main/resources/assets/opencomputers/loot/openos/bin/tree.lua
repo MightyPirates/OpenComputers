ad37d53d1035767ec0b0e46a42158450f2b9c960
LeshaInc <includeurl@gmail.com>
2017-07-05 22:53:26 +0300

    Add tree utility

---
+++ src/main/resources/assets/opencomputers/loot/openos/bin/tree.lua
@@ -0,0 +1,274 @@
+local shell = require("shell")
+local fs = require("filesystem")
+
+local args, opts = shell.parse(...)
+
+local function die(...)
+  io.stderr:write(...)
+  os.exit(1)
+end
+
+do -- handle cli
+  if opts.h or opts.help then
+    print([[Usage: tree [OPTION]... [FILE]...
+  -a, --all             do not ignore entries starting with .
+      --full-time       with -l, print time in full iso format
+  -h, --human-readable  with -l, print human readable sizes
+      --si              likewise, but use powers of 1000 not 1024
+      --level=LEVEL     descend only LEVEL directories deep
+      --color[=WHEN]    [WIP] WHEN can be
+                        auto - colorize output only if writing to a tty,
+                        always - always colorize output,
+                        never - never colorize output; (default: auto)
+  -l                    use a long listing format
+  -f                    print the full path prefix for each file
+  -i                    do not print indentation lines
+  -p                    append "/" indicator to directories
+  -Q, --quote           quote filenames with double quotes
+  -r, --reverse         reverse order while sorting
+  -S                    sort by file size
+  -t                    sort by modification type, newest first
+  -X                    sort alphabetically by entry extension
+  -C                    do not count files and directories
+  -R                    count root directories like other files
+      --help            print this help and exit]])
+    return 0
+  end
+
+  if #args == 0 then
+    table.insert(args, ".")
+  end
+
+  opts.level = tonumber(opts.level) or math.huge
+  if opts.level < 1 then
+    die("Invalid level, must be greater than 0")
+  end
+end
+
+local function peekable(iterator, state, var1)
+  local nextItem = {iterator(state, var1)}
+
+  return setmetatable({
+    peek = function()
+      return table.unpack(nextItem)
+    end
+  }, {
+    __call = coroutine.wrap(function()
+      while true do
+        local item = nextItem
+        nextItem = {iterator(state, nextItem[1])}
+        coroutine.yield(table.unpack(item))
+        if nextItem[1] == nil then break end
+      end
+    end)
+  })
+end
+
+local function filter(entry)
+  return opts.a or entry:sub(1, 1) ~= "."
+end
+
+local function stat(path)
+  local st = {}
+  st.path = path
+  st.name = fs.name(path)
+  st.sortName = st.name:gsub("^%.","")
+  st.time = fs.lastModified(path)
+  st.isLink = fs.isLink(path)
+  st.isDirectory = fs.isDirectory(path)
+  st.size = st.isLink and 0 or fs.size(path)
+  st.extension = st.name:match("(%.[^.]+)$") or ""
+  st.fs = fs.get(path)
+  return st
+end
+
+local function list(path)
+  return coroutine.wrap(function()
+    local l = {}
+    for entry in fs.list(path) do
+      if filter(entry) then
+        table.insert(l, stat(fs.concat(path, entry)))
+      end
+    end
+
+    if opts.S then
+      table.sort(l, function(a, b)
+        return a.size < b.size
+      end)
+    elseif opts.t then
+      table.sort(l, function(a, b)
+        return a.time < b.time
+      end)
+    elseif opts.X then
+      table.sort(l, function(a, b)
+        return a.extension < b.extension
+      end)
+    else
+      table.sort(l, function(a, b)
+        return a.sortName < b.sortName
+      end)
+    end
+
+    for i = opts.r and #l or 1, opts.r and 1 or #l, opts.r and -1 or 1 do
+      coroutine.yield(l[i])
+    end
+  end)
+end
+
+local function digRoot(root)
+  local rootPath = fs.realPath(shell.resolve(root))
+  coroutine.yield(stat(rootPath), {})
+
+  if not fs.isDirectory(rootPath) then
+    return
+  end
+
+  local iterStack = {peekable(list(rootPath))}
+  local pathStack = {rootPath}
+  local levelStack = {not not iterStack[#iterStack]:peek()}
+
+  repeat
+    local entry = iterStack[#iterStack]()
+
+    if entry then
+      levelStack[#levelStack] = not not iterStack[#iterStack]:peek()
+
+      local path = fs.concat(fs.concat(table.unpack(pathStack)), entry.name)
+
+      coroutine.yield(entry, levelStack)
+
+      if entry.isDirectory and opts.level > #levelStack then
+        table.insert(iterStack, peekable(list(path)))
+        table.insert(pathStack, entry.name)
+        table.insert(levelStack, not not iterStack[#iterStack]:peek())
+      end
+    else
+      table.remove(iterStack)
+      table.remove(pathStack)
+      table.remove(levelStack)
+    end
+  until #iterStack == 0
+end
+
+local function dig(roots)
+  return coroutine.wrap(function()
+    for _, root in ipairs(roots) do
+      digRoot(root)
+    end
+  end)
+end
+
+local function nod(n)  -- from /lib/core/full_ls.lua
+  return n and (tostring(n):gsub("(%.[0-9]+)0+$","%1")) or "0"
+end
+
+local function formatFSize(size)  -- from /lib/core/full_ls.lua
+  if not ops.h and not ops["human-readable"] and not ops.si then
+    return tostring(size)
+  end
+
+  local sizes = {"", "K", "M", "G"}
+  local unit = 1
+  local power = ops.si and 1000 or 1024
+
+  while size > power and unit < #sizes do
+    unit = unit + 1
+    size = size / power
+  end
+
+  return nod(math.floor(size*10)/10)..sizes[unit]
+end
+
+local function pad(txt)  -- from /lib/core/full_ls.lua
+  txt = tostring(txt)
+  return #txt >= 2 and txt or "0" .. txt
+end
+
+local function formatTime(epochms)  -- from /lib/core/full_ls.lua
+  local month_names = {"January","February","March","April","May","June",
+     "July","August","September","October","November","December"}
+
+  if epochms == 0 then return "" end
+
+  local d = os.date("*t", epochms)
+  local day, hour, min, sec = nod(d.day), pad(nod(d.hour)), pad(nod(d.min)), pad(nod(d.sec))
+
+  if ops["full-time"] then
+    return string.format("%s-%s-%s %s:%s:%s ", d.year, pad(nod(d.month)), pad(day), hour, min, sec)
+  else
+    return string.format("%s %+2s %+2s:%+2s ", month_names[d.month]:sub(1,3), day, hour, pad(min))
+  end
+end
+
+local function writeEntry(entry, levelStack)
+  for i, hasNext in ipairs(levelStack) do
+    if opts.i then break end
+
+    if i == #levelStack then
+      if hasNext then
+        io.write("├── ")
+      else
+        io.write("└── ")
+      end
+    else
+      if hasNext then
+        io.write("│   ")
+      else
+        io.write("    ")
+      end
+    end
+  end
+
+  if opts.l then
+    io.write("[")
+
+    io.write(entry.isDirectory and "d" or entry.isLink and "l" or "f", "-")
+    io.write("r", entry.fs.isReadOnly() and "-" or "w", " ")
+
+    io.write(formatFSize(entry.size), " ")
+
+    io.write(formatTime(entry.time))
+    io.write("] ")
+  end
+
+  if opts.Q then io.write('"') end
+
+  if opts.f then
+    io.write(entry.path)
+  else
+    io.write(entry.name)
+  end
+
+  if opts.p and entry.isDirectory then
+    io.write("/")
+  end
+
+  if opts.Q then io.write('"') end
+  io.write("\n")
+end
+
+local function writeCount(dirs, files)
+  io.write("\n")
+  io.write(dirs, " director", dirs == 1 and "y" or "ies")
+  io.write(", ")
+  io.write(files, " file", files == 1 and "" or "s")
+  io.write("\n")
+end
+
+local dirs, files = 0, 0
+
+for entry, levelStack in dig(args) do
+  if opts.R or #levelStack > 0 then
+    if entry.isDirectory then
+      dirs = dirs + 1
+    else
+      files = files + 1
+    end
+  end
+  writeEntry(entry, levelStack)
+end
+
+if not opts.C then
+  writeCount(dirs, files)
+end
+
