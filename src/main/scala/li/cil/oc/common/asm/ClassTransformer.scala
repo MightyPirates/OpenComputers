72e216f9f617f3babd6b4832ecacd8b24ddd0fda
modmuss50 <modmuss50@gmail.com>
2017-07-18 22:37:11 +0100

    Update obf names in core mod.

--- src/main/scala/li/cil/oc/common/asm/ClassTransformer.scala
+++ src/main/scala/li/cil/oc/common/asm/ClassTransformer.scala
@@ -16,13 +16,13 @@ import scala.collection.convert.WrapAsJava._
 import scala.collection.convert.WrapAsScala._
 
 object ObfNames {
-  final val Class_EntityHanging = Array("net/minecraft/entity/EntityHanging", "yy")
-  final val Class_EntityLiving = Array("net/minecraft/entity/EntityLiving", "sx")
-  final val Class_RenderLiving = Array("net/minecraft/client/renderer/entity/RenderLiving", "bvq")
-  final val Class_TileEntity = Array("net/minecraft/tileentity/TileEntity", "asc")
-  final val Field_leashNBTTag = Array("leashNBTTag", "field_110170_bx", "bF")
-  final val Field_leashedToEntity = Array("leashedToEntity", "field_110168_bw", "bE")
-  final val Method_recreateLeash = Array("recreateLeash", "func_110165_bF", "da")
+  final val Class_EntityHanging = Array("net/minecraft/entity/EntityHanging", "aby")
+  final val Class_EntityLiving = Array("net/minecraft/entity/EntityLiving", "vo")
+  final val Class_RenderLiving = Array("net/minecraft/client/renderer/entity/RenderLiving", "cad")
+  final val Class_TileEntity = Array("net/minecraft/tileentity/TileEntity", "avh")
+  final val Field_leashNBTTag = Array("leashNBTTag", "field_110170_bx", "bG")
+  final val Field_leashedToEntity = Array("leashedToEntity", "field_110168_bw", "bF")
+  final val Method_recreateLeash = Array("recreateLeash", "func_110165_bF", "de")
   final val Method_recreateLeashDesc = Array("()V")
   final val Method_renderLeash = Array("renderLeash", "func_110827_b", "b")
   final val Method_renderLeashDesc = Array("(Lsg;DDDFF)V", "(Lnet/minecraft/entity/EntityLiving;DDDFF)V")
