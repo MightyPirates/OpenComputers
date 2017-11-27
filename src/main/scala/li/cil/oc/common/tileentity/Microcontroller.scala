4a9b24c9b7c40bfe756a7e2ca29717f5038afccf
payonel <payonel@hotmail.com>
2017-11-27 13:56:36 -0800

    merge oops

--- src/main/scala/li/cil/oc/common/tileentity/Microcontroller.scala
+++ src/main/scala/li/cil/oc/common/tileentity/Microcontroller.scala
@@ -261,18 +261,11 @@ class Microcontroller extends traits.PowerAcceptor with traits.Hub with traits.C
   override def removeStackFromSlot(slot: Int) = null
 
   // Nope.
-  override def getAccessibleSlotsFromSide(side: Int): Array[Int] = Array()
+  override def canExtractItem(slot: Int, stack: ItemStack, side: EnumFacing) = false
 
-  override def canExtractItem(slot: Int, stack: ItemStack, side: Int) = false
+  override def canInsertItem(slot: Int, stack: ItemStack, side: EnumFacing) = false
 
-  override def canInsertItem(slot: Int, stack: ItemStack, side: Int) = false
-
-  // Nope.
-  override def getAccessibleSlotsFromSide(side: Int): Array[Int] = Array()
-
-  override def canExtractItem(slot: Int, stack: ItemStack, side: Int) = false
-
-  override def canInsertItem(slot: Int, stack: ItemStack, side: Int) = false
+  override def getSlotsForFace(side: EnumFacing): Array[Int] = Array()
 
   // For hotswapping EEPROMs.
   def changeEEPROM(newEeprom: ItemStack) = {
