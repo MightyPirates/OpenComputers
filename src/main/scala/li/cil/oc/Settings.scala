1b02768de2cfc0c54048e13853065e69c5ed406d
TheCodex6824 <thecodex6824@gmail.com>
2017-12-11 14:56:53 -0500

    Bump version number for config updates Fix typo in config comment

--- src/main/scala/li/cil/oc/Settings.scala
+++ src/main/scala/li/cil/oc/Settings.scala
@@ -542,9 +542,9 @@ object Settings {
     VersionRange.createFromVersionSpec("[0.0, 1.5.20)") -> Array(
       "switch.relayDelayUpgrade"
     ),
-    // Upgrading past version 1.7.0 (?), changed wireless card stuff for t1 card.
+    // Upgrading past version 1.7.1 (?), changed wireless card stuff for t1 card.
     // TODO Update next version when its known what it will actually be
-    VersionRange.createFromVersionSpec("[0.0, 1.7.1)") -> Array(
+    VersionRange.createFromVersionSpec("[0.0, 1.7.2)") -> Array(
       "power.cost.wirelessCostPerRange",
       "misc.maxWirelessRange",
       "misc.maxOpenPorts"
