26b127e74f8a7a3f356a451c50bdd274299b911e
TheCodex6824 <thecodex6824@gmail.com>
2018-02-13 19:08:18 -0500

    Fixed drones not reclaiming their chunk tickets (#2727)
    If a ticket is created with ForgeChunkManager.Type.ENTITY and bound to
    an entity, then forge will actually load that entity's chunk before
    returning its ticket. As a result, the drones were initializing their
    components before the ticket was added to restoredTickets, so they
    never actually reclaimed their ticket. Also added a log message to
    detect if something like this happens again.

--- src/main/scala/li/cil/oc/server/component/UpgradeChunkloader.scala
+++ src/main/scala/li/cil/oc/server/component/UpgradeChunkloader.scala
@@ -68,7 +68,7 @@ class UpgradeChunkloader(val host: EnvironmentHost) extends prefab.ManagedEnviro
         OpenComputers.log.info(s"Reclaiming chunk loader ticket at (${host.xPosition()}, ${host.yPosition()}, ${host.zPosition()}) in dimension ${host.world().provider.dimensionId}.")
       }
       ticket = ChunkloaderUpgradeHandler.restoredTickets.remove(node.address).orElse(host match {
-        case context: Context if context.isRunning => Option(ForgeChunkManager.requestTicket(OpenComputers, host.world, if (host.isInstanceOf[Entity]) ForgeChunkManager.Type.ENTITY else ForgeChunkManager.Type.NORMAL))
+        case context: Context if context.isRunning => Option(ForgeChunkManager.requestTicket(OpenComputers, host.world, ForgeChunkManager.Type.NORMAL))
         case _ => None
       })
       ChunkloaderUpgradeHandler.updateLoadedChunk(this)
@@ -97,7 +97,7 @@ class UpgradeChunkloader(val host: EnvironmentHost) extends prefab.ManagedEnviro
 
   private def setActive(enabled: Boolean) = {
     if (enabled && ticket.isEmpty) {
-      ticket = Option(ForgeChunkManager.requestTicket(OpenComputers, host.world, if (host.isInstanceOf[Entity]) ForgeChunkManager.Type.ENTITY else ForgeChunkManager.Type.NORMAL))
+      ticket = Option(ForgeChunkManager.requestTicket(OpenComputers, host.world, ForgeChunkManager.Type.NORMAL))
       ChunkloaderUpgradeHandler.updateLoadedChunk(this)
     }
     else if (!enabled && ticket.isDefined) {
