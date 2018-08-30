a85fe69692e8a83d3698254087a489b122be3348
payonel <payonel@hotmail.com>
2018-08-30 12:45:11 -0700

    using java 7 syntax to fix our IDEA builds

--- src/main/scala/li/cil/oc/common/capabilities/Capabilities.java
+++ src/main/scala/li/cil/oc/common/capabilities/Capabilities.java
@@ -7,6 +7,8 @@ import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 
+import java.util.concurrent.Callable;
+
 // Gotta be Java, @CapabilityInject don't werk for Scala ;_;
 public final class Capabilities {
     @CapabilityInject(Colored.class)
@@ -18,10 +20,27 @@ public final class Capabilities {
     @CapabilityInject(SidedEnvironment.class)
     public static Capability<SidedEnvironment> SidedEnvironmentCapability;
 
+    // java 7 doesn't have generic type constraints
+    // java 7 doesn't have lambdas
+    // java 7 doesn't generic type covariance
+    private static class StupidJavaTookTooManyYearsToIntroduceLambdas<T> implements Callable<T> {
+
+        StupidJavaTookTooManyYearsToIntroduceLambdas(Class cls) {
+            _cls = cls;
+        }
+
+        @Override
+        public T call() throws Exception {
+            return (T)_cls.newInstance();
+        }
+
+        private Class _cls;
+    }
+
     public static void init() {
-        CapabilityManager.INSTANCE.register(Environment.class, new CapabilityEnvironment.DefaultStorage(), CapabilityEnvironment.DefaultImpl::new);
-        CapabilityManager.INSTANCE.register(SidedEnvironment.class, new CapabilitySidedEnvironment.DefaultStorage(), CapabilitySidedEnvironment.DefaultImpl::new);
-        CapabilityManager.INSTANCE.register(Colored.class, new CapabilityColored.DefaultStorage(), CapabilityColored.DefaultImpl::new);
+        CapabilityManager.INSTANCE.register(Environment.class, new CapabilityEnvironment.DefaultStorage(), new StupidJavaTookTooManyYearsToIntroduceLambdas<Environment>(CapabilityEnvironment.DefaultImpl.class));
+        CapabilityManager.INSTANCE.register(SidedEnvironment.class, new CapabilitySidedEnvironment.DefaultStorage(), new StupidJavaTookTooManyYearsToIntroduceLambdas<SidedEnvironment>(CapabilitySidedEnvironment.DefaultImpl.class));
+        CapabilityManager.INSTANCE.register(Colored.class, new CapabilityColored.DefaultStorage(), new StupidJavaTookTooManyYearsToIntroduceLambdas<Colored>(CapabilityColored.DefaultImpl.class));
     }
 
     private Capabilities() {
