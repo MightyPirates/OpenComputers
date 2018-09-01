1fb05ff432e57468b2137fb5371361cca8d507ab
payonel <payonel@hotmail.com>
2018-09-01 15:07:33 -0700

    fix and simplify /bin/more

--- src/main/resources/assets/opencomputers/loot/openos/bin/more.lua
+++ src/main/resources/assets/opencomputers/loot/openos/bin/more.lua
@@ -1,7 +1,7 @@
 local buffer = require("buffer")
 local keyboard = require("keyboard")
 local shell = require("shell")
-local tty = require("tty")
+local term = require("term")
 
 local args = shell.parse(...)
 if #args > 1 then
@@ -10,48 +10,36 @@ if #args > 1 then
   return 1
 end
 
-local function clear_line()
-  tty.window.x = 1 -- move cursor to start of line
-  io.write("\27[2K") -- clear line
+local cat_cmd = table.concat({"cat", ...}, " ")
+if not io.output().tty then
+  return os.execute(cat_cmd)
 end
 
-if io.output().tty then
-  io.write("\27[2J\27[H")
+term.clear()
 
-  local intercept_active = true
-  local original_stream = io.stdout.stream
-  local custom_stream = setmetatable({
-    scroll = function(...)
-      local _, height, _, _, _, y = tty.getViewport()
-      local lines_below = height - y
-      if intercept_active and lines_below < 1 then
-        intercept_active = false
-        original_stream.scroll(-lines_below) -- if zero no scroll action is made [good]
-        tty.setCursor(1, height) -- move to end
-        clear_line()
-        io.write(":") -- status
-        local _, _, _, code = original_stream:pull(nil, "key_down") -- nil timeout is math.huge
-        if code == keyboard.keys.q then
-          clear_line()
-          os.exit(1) -- abort
-        elseif code == keyboard.keys["end"] then
-          io.stdout.stream.scroll = nil -- remove handler
-        elseif code == keyboard.keys.space or code == keyboard.keys.pageDown then
-          io.write("\27[2J\27[H") -- clear whole screen, get new page drawn; move cursor to 1,1
-        elseif code == keyboard.keys.enter or code == keyboard.keys.down then
-          clear_line() -- remove status bar
-          original_stream.scroll(1) -- move everything up one
-          tty.setCursor(1, height - 1)
-        end
-        intercept_active = true
-      end
-      return original_stream.scroll(...)
+local preader = io.popen(cat_cmd)
+local intercept = true
+while true do
+  local _, height, _, _, _, y = term.getViewport()
+  if intercept and y == height then
+    term.clearLine()
+    io.write(":") -- status
+    ::INPUT::
+    local _, _, _, code = term.pull("key_down")
+    if code == keyboard.keys.q then
+      term.clearLine()
+      os.exit(1) -- abort
+    elseif code == keyboard.keys["end"] then
+      intercept = false
+    elseif code == keyboard.keys.space or code == keyboard.keys.pageDown then
+      term.clear() -- clear whole screen, get new page drawn; move cursor to 1,1
+    elseif code == keyboard.keys.enter or code == keyboard.keys.down then
+      term.clearLine() -- remove status bar
+      term.scroll(1) -- move everything up one
+      term.setCursor(1, height - 1)
+    else
+      goto INPUT
     end
-  }, {__index=original_stream})
-
-  local custom_output_buffer = buffer.new("w", custom_stream)
-  custom_output_buffer:setvbuf("no")
-  io.output(custom_output_buffer)
+  end
+  print(preader:read() or os.exit())
 end
-
-return loadfile(shell.resolve("cat", "lua"))(...)
