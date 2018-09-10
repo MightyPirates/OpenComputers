225e907c49af61c8c3357ebd1ca8a43ea96127d4
Vexatos <Stuarzt@gmx.de>
2018-09-10 17:56:41 +0200

    Replace ChatFormatting with TextFormatting, like it should be. ChatFormatting only exists on the client side. Fixes #2792. Fixes #2850.

--- src/main/scala/li/cil/oc/client/renderer/markdown/segment/StrikethroughSegment.scala
+++ src/main/scala/li/cil/oc/client/renderer/markdown/segment/StrikethroughSegment.scala
@@ -1,10 +1,10 @@
 package li.cil.oc.client.renderer.markdown.segment
 
-import com.mojang.realmsclient.gui.ChatFormatting
 import li.cil.oc.client.renderer.markdown.MarkupFormat
+import net.minecraft.util.text.TextFormatting
 
 private[markdown] class StrikethroughSegment(parent: Segment, text: String) extends TextSegment(parent, text) {
-  override protected def format = ChatFormatting.STRIKETHROUGH.toString
+  override protected def format = TextFormatting.STRIKETHROUGH.toString
 
   override def toString(format: MarkupFormat.Value): String = format match {
     case MarkupFormat.Markdown => s"~~$text~~"
