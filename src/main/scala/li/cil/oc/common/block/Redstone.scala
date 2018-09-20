389778cd2fd65d90bcb75032f4aba191c52a9235
payonel <payonel@hotmail.com>
2018-09-19 22:21:33 -0700

    the good build properties and tooltip

--- src/main/scala/li/cil/oc/common/block/Redstone.scala
+++ src/main/scala/li/cil/oc/common/block/Redstone.scala
@@ -14,9 +14,9 @@ class Redstone extends RedstoneAware {
   override protected def tooltipTail(metadata: Int, stack: ItemStack, world: World, tooltip: util.List[String], advanced: ITooltipFlag) {
     super.tooltipTail(metadata, stack, world, tooltip, advanced)
     // todo more generic way for redstone mods to provide lines
-//    if (Mods.ProjectRedTransmission.isAvailable) {
-//      tooltip.addAll(Tooltip.get("RedstoneCard.ProjectRed"))
-//    }
+    if (Mods.ProjectRedTransmission.isModAvailable) {
+      tooltip.addAll(Tooltip.get("RedstoneCard.ProjectRed"))
+    }
   }
 
   // ----------------------------------------------------------------------- //
