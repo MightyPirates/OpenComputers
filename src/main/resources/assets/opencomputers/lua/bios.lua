72e096e2c86572091d9d0c3ae57948d6e6573312
payonel <payonel@hotmail.com>
2018-11-16 02:51:45 -0800

    reduce some memory cost used by our default eeprom

--- src/main/resources/assets/opencomputers/lua/bios.lua
+++ src/main/resources/assets/opencomputers/lua/bios.lua
@@ -1,61 +1,64 @@
-local component_invoke = component.invoke
-function boot_invoke(address, method, ...)
-  local result = table.pack(pcall(component_invoke, address, method, ...))
-  if not result[1] then
-    return nil, result[2]
-  else
-    return table.unpack(result, 2, result.n)
-  end
-end
-
--- backwards compatibility, may remove later
-local eeprom = component.list("eeprom")()
-computer.getBootAddress = function()
-  return boot_invoke(eeprom, "getData")
-end
-computer.setBootAddress = function(address)
-  return boot_invoke(eeprom, "setData", address)
-end
-
+local init
 do
-  local screen = component.list("screen")()
-  local gpu = component.list("gpu")()
-  if gpu and screen then
-    boot_invoke(gpu, "bind", screen)
+  local component_invoke = component.invoke
+  local function boot_invoke(address, method, ...)
+    local result = table.pack(pcall(component_invoke, address, method, ...))
+    if not result[1] then
+      return nil, result[2]
+    else
+      return table.unpack(result, 2, result.n)
+    end
   end
-end
-local function tryLoadFrom(address)
-  local handle, reason = boot_invoke(address, "open", "/init.lua")
-  if not handle then
-    return nil, reason
+
+  -- backwards compatibility, may remove later
+  local eeprom = component.list("eeprom")()
+  computer.getBootAddress = function()
+    return boot_invoke(eeprom, "getData")
   end
-  local buffer = ""
-  repeat
-    local data, reason = boot_invoke(address, "read", handle, math.huge)
-    if not data and reason then
+  computer.setBootAddress = function(address)
+    return boot_invoke(eeprom, "setData", address)
+  end
+
+  do
+    local screen = component.list("screen")()
+    local gpu = component.list("gpu")()
+    if gpu and screen then
+      boot_invoke(gpu, "bind", screen)
+    end
+  end
+  local function tryLoadFrom(address)
+    local handle, reason = boot_invoke(address, "open", "/init.lua")
+    if not handle then
       return nil, reason
     end
-    buffer = buffer .. (data or "")
-  until not data
-  boot_invoke(address, "close", handle)
-  return load(buffer, "=init")
-end
-local init, reason
-if computer.getBootAddress() then
-  init, reason = tryLoadFrom(computer.getBootAddress())
-end
-if not init then
-  computer.setBootAddress()
-  for address in component.list("filesystem") do
-    init, reason = tryLoadFrom(address)
-    if init then
-      computer.setBootAddress(address)
-      break
+    local buffer = ""
+    repeat
+      local data, reason = boot_invoke(address, "read", handle, math.huge)
+      if not data and reason then
+        return nil, reason
+      end
+      buffer = buffer .. (data or "")
+    until not data
+    boot_invoke(address, "close", handle)
+    return load(buffer, "=init")
+  end
+  local reason
+  if computer.getBootAddress() then
+    init, reason = tryLoadFrom(computer.getBootAddress())
+  end
+  if not init then
+    computer.setBootAddress()
+    for address in component.list("filesystem") do
+      init, reason = tryLoadFrom(address)
+      if init then
+        computer.setBootAddress(address)
+        break
+      end
     end
   end
+  if not init then
+    error("no bootable medium found" .. (reason and (": " .. tostring(reason)) or ""), 0)
+  end
+  computer.beep(1000, 0.2)
 end
-if not init then
-  error("no bootable medium found" .. (reason and (": " .. tostring(reason)) or ""), 0)
-end
-computer.beep(1000, 0.2)
 init()
