c97a985fc12d8788656cdcb3db83e46669d62734
payonel <payonel@hotmail.com>
2019-05-29 00:30:05 -0700

    return non-zero when block breaks to air too fast closes #3050

--- src/main/scala/li/cil/oc/server/agent/Player.scala
+++ src/main/scala/li/cil/oc/server/agent/Player.scala
@@ -423,8 +423,12 @@ class Player(val agent: internal.Agent) extends FakePlayer(agent.world.asInstanc
     if (preEvent.isCanceled) return 0
     val adjustedBreakTime = Math.max(0.05, preEvent.getBreakTime)
 
-    if (!PlayerInteractionManagerHelper.onBlockClicked(this, pos, side))
+    if (!PlayerInteractionManagerHelper.onBlockClicked(this, pos, side)) {
+      if (world.isAirBlock(pos)) {
+        return 1.0 / 20.0
+      }
       return 0
+    }
 
     EventHandler.scheduleServer(() => new DamageOverTime(this, pos, side, (adjustedBreakTime * 20).toInt).tick())
 
