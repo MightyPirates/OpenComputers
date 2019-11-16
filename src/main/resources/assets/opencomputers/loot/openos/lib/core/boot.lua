343ce9687a526b753922ca51d5a7196d58b13044
Vexatos <Stuarzt@gmx.de>
2019-11-17 00:40:38 +0100

    Version and changelog bump.

--- src/main/resources/assets/opencomputers/loot/openos/lib/core/boot.lua
+++ src/main/resources/assets/opencomputers/loot/openos/lib/core/boot.lua
@@ -1,7 +1,7 @@
 -- called from /init.lua
 local raw_loadfile = ...
 
-_G._OSVERSION = "OpenOS 1.7.4"
+_G._OSVERSION = "OpenOS 1.7.5"
 
 -- luacheck: globals component computer unicode _OSVERSION
 local component = component
