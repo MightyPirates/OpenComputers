09505e6cff410fb1bfd822eebb2b0f61f52024b3
payonel <payonel@hotmail.com>
2020-05-21 00:36:09 -0700

    openos improvements 1. if /home is readonly, a helpful message is displayed tell the user to run install 2. remove -i from `cp` alias because a bunch of people complain about it 3. `install` now does not clobber /etc/rc.cfg nor /home/.shrc

--- src/main/resources/assets/opencomputers/loot/openos/lib/core/install_basics.lua
+++ src/main/resources/assets/opencomputers/loot/openos/lib/core/install_basics.lua
@@ -27,7 +27,7 @@ local utils
 
 local rootfs = fs.get("/")
 if not rootfs then
-  io.stderr:write("no root filesystem, aborting\n");
+  io.stderr:write("no root filesystem, aborting\n")
   os.exit(1)
 end
 
@@ -86,11 +86,13 @@ for dev, path in pairs(devices) do
       io.stderr:write("Cannot install to " .. options.to .. ", it is read only\n")
       os.exit(1)
     end
-  elseif specified or
-    not (source_filter and address:find(source_filter, 1, true) == 1) and -- specified for source
-    not target_filter and
-    address ~= tmpAddress then
-    table.insert(targets, {dev=dev, path=install_path, specified=specified})
+  elseif
+    specified or
+      not (source_filter and address:find(source_filter, 1, true) == 1) and -- specified for source
+        not target_filter and
+        address ~= tmpAddress
+   then
+    table.insert(targets, {dev = dev, path = install_path, specified = specified})
   end
 end
 
@@ -105,11 +107,11 @@ for dev, path in pairs(devices) do
   local install_path = dev == source_filter_dev and options.from or path
   local specified = source_filter and address:find(source_filter, 1, true) == 1
 
-  if fs.list(install_path)()
-    and (specified or 
-      not source_filter and
-      address ~= tmpAddress and
-      not (address == rootfs.address and not rootfs.isReadOnly())) then
+  if
+    fs.list(install_path)() and
+      (specified or
+        not source_filter and address ~= tmpAddress and not (address == rootfs.address and not rootfs.isReadOnly()))
+   then
     local prop = {}
     local prop_path = path .. "/.prop"
     local prop_file = fs.open(prop_path)
@@ -125,7 +127,7 @@ for dev, path in pairs(devices) do
     end
     if not prop.ignore then
       if not label or label:lower() == (prop.label or dev.getLabel() or ""):lower() then
-        table.insert(sources, {dev=dev, path=install_path, prop=prop, specified=specified})
+        table.insert(sources, {dev = dev, path = install_path, prop = prop, specified = specified})
       end
     end
   end
@@ -137,23 +139,24 @@ if #sources ~= 1 then
   utils = loadfile(utils_path, "bt", _G)
   source = utils("select", "sources", options, sources)
 end
-if not source then return end
+if not source then
+  return
+end
 
-options =
-{
-  from     = source.path .. '/',
-  fromDir  = fs.canonical(options.fromDir or source.prop.fromDir or ""),
-  root     = fs.canonical(options.root or options.toDir or source.prop.root or ""),
-  update   = options.update or options.u,
-  label    = source.prop.label or label,
+options = {
+  from = source.path .. "/",
+  fromDir = fs.canonical(options.fromDir or source.prop.fromDir or ""),
+  root = fs.canonical(options.root or options.toDir or source.prop.root or ""),
+  update = options.update or options.u,
+  label = source.prop.label or label,
   setlabel = not (options.nosetlabel or options.nolabelset) and source.prop.setlabel,
-  setboot  = not (options.nosetboot or options.noboot) and source.prop.setboot,
-  reboot   = not options.noreboot and source.prop.reboot,
+  setboot = not (options.nosetboot or options.noboot) and source.prop.setboot,
+  reboot = not options.noreboot and source.prop.reboot
 }
 local source_display = options.label or source.dev.getLabel() or source.path
 
 -- Remove the source from the target options
-for index,entry in ipairs(targets) do
+for index, entry in ipairs(targets) do
   if entry.dev == source.dev then
     table.remove(targets, index)
     target = targets[1]
@@ -162,47 +165,67 @@ end
 
 -- Ask the user to select a target
 if #targets ~= 1 then
-    if #sources == 1 then
-      io.write(source_display, " selected for install\n")
-    end
+  if #sources == 1 then
+    io.write(source_display, " selected for install\n")
+  end
 
-    utils = utils or loadfile(utils_path, "bt", _G)
+  utils = utils or loadfile(utils_path, "bt", _G)
   target = utils("select", "targets", options, targets)
 end
-if not target then return end
+if not target then
+  return
+end
 
-options.to       = target.path .. '/'
+options.to = target.path .. "/"
 
-local cp_args =
-{
-  "-vrx" .. (options.update and "ui" or ""),
-  "--skip=.prop",
-  fs.concat(options.from, options.fromDir) .. "/.",
-  fs.concat(options.to  , options.root)
+local function resolveFrom(path)
+  return fs.concat(options.from, options.fromDir) .. "/" .. path
+end
+
+local fullTargetPath = fs.concat(options.to, options.root)
+local transfer_args = {
+  {
+    {resolveFrom("."), fullTargetPath},
+    {
+      cmd = "cp",
+      r = true, v = true, x = true, u = options.update, i = options.update,
+      skip = {resolveFrom(".prop")},
+    }
+  }
 }
+
+if source.prop.noclobber and #source.prop.noclobber > 0 then
+  local noclobber_opts = {cmd = "cp", v = true, n = true}
+  for _, noclobber in ipairs(source.prop.noclobber or {}) do
+    local noclobberFrom = resolveFrom(noclobber)
+    local noclobberTo = fs.concat(fullTargetPath, noclobber)
+    table.insert(transfer_args[1][2].skip, noclobberFrom)
+    table.insert(transfer_args, {{noclobberFrom, noclobberTo}, noclobber_opts})
+  end
+end
 
 local special_target = ""
 if #targets > 1 or target_filter or source_filter then
-  special_target = " to " .. cp_args[4]
+  special_target = " to " .. transfer_args[1].args[2]
 end
 
 io.write("Install " .. source_display .. special_target .. "? [Y/n] ")
-if not ((io.read() or "n").."y"):match("^%s*[Yy]") then
+if not ((io.read() or "n") .. "y"):match("^%s*[Yy]") then
   io.write("Installation cancelled\n")
   os.exit()
 end
 
 local installer_path = options.from .. "/.install"
 if fs.exists(installer_path) then
-  local installer, reason = loadfile(installer_path, "bt", setmetatable({install=options}, {__index = _G}))
+  local installer, reason = loadfile(installer_path, "bt", setmetatable({install = options}, {__index = _G}))
   if not installer then
-    io.stderr:write("installer failed to load: " .. tostring(reason) .. '\n')
+    io.stderr:write("installer failed to load: " .. tostring(reason) .. "\n")
     os.exit(1)
   end
   os.exit(installer())
 end
 
-options.cp_args = cp_args
+options.cp_args = transfer_args
 options.target = target
 
 return options
