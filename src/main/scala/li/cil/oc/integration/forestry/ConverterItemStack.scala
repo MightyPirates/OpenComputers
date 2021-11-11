fac6f3f0477bb57fde1a365f99e2c315a76cc369
repo_alt <wvk17@yandex.ru>
2021-11-11 18:38:13 +0300

    Forestry circuit boards internal layout https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues/8809

--- src/main/scala/li/cil/oc/integration/forestry/ConverterItemStack.scala
+++ src/main/scala/li/cil/oc/integration/forestry/ConverterItemStack.scala
@@ -2,6 +2,7 @@ package li.cil.oc.integration.forestry
 
 import java.util
 
+import forestry.api.circuits.{ChipsetManager, ICircuit}
 import forestry.api.genetics.AlleleManager
 import li.cil.oc.api.driver.Converter
 import net.minecraft.item.ItemStack
@@ -12,6 +13,12 @@ object ConverterItemStack extends Converter {
   override def convert(value: scala.Any, output: util.Map[AnyRef, AnyRef]): Unit = value match {
     case stack: ItemStack if AlleleManager.alleleRegistry.isIndividual(stack) =>
       output += "individual" -> AlleleManager.alleleRegistry.getIndividual(stack)
+    case stack: ItemStack if ChipsetManager.circuitRegistry.getCircuitboard(stack) != null => {
+      val cc = ChipsetManager.circuitRegistry.getCircuitboard(stack).getCircuits
+      val names = cc.collect{case c: ICircuit => c.getName}
+      if (names.length > 0)
+        output += "circuits" -> names
+    }
     case _ =>
   }
 }
