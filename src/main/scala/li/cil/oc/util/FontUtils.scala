23d218d598a31799a24cd155333675a4fb126414
Adrian Siekierka <kontakt@asie.pl>
2022-06-07 17:03:49 +0200

    update font to unscii 2.1 + funscii patches + unifont 14.0.04 (thanks to the OFL license option)

--- src/main/scala/li/cil/oc/util/FontUtils.scala
+++ src/main/scala/li/cil/oc/util/FontUtils.scala
@@ -165,14 +165,11 @@ object FontUtils {
               case n => OpenComputers.log.warn(s"Invalid glyph size detected in font.hex. Expected 64 or 32, got: $n")
             }
           } else {
-            if (out_of_range_glyph == 0) {
-              OpenComputers.log.warn(f"Invalid glyph char code detected in font.hex. Expected 0<= charCode <$codepoint_limit%X, got: $charCode%X")
-            }
             out_of_range_glyph += 1
           }
         }
         if (out_of_range_glyph > 1) {
-          OpenComputers.log.warn(f"${out_of_range_glyph} total invalid glyph char codes detected in font.hex")
+          OpenComputers.log.info(f"${out_of_range_glyph} total non-BMP glyph char codes detected in font.hex")
         }
       } finally {
         try {
