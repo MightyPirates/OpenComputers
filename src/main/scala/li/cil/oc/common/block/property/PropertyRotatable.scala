790f9f636a9f8053826d848d5aae0d3ae95aaf91
KosmosPrime <5663514+KosmosPrime@users.noreply.github.com>
2022-07-25 22:58:11 +0200

    Fix crash due to changed property ABI

--- src/main/scala/li/cil/oc/common/block/property/PropertyRotatable.scala
+++ src/main/scala/li/cil/oc/common/block/property/PropertyRotatable.scala
@@ -1,14 +1,14 @@
 package li.cil.oc.common.block.property
 
-import com.google.common.base.Predicate
-import com.google.common.base.Predicates
+import java.util.function.Predicate
+
 import net.minecraft.state.DirectionProperty
 import net.minecraft.util.Direction
 
 import scala.collection.convert.ImplicitConversionsToJava._
 
 object PropertyRotatable {
-  final val Facing = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL.asInstanceOf[Predicate[Direction]])
-  final val Pitch = DirectionProperty.create("pitch", Predicates.in(Set(Direction.DOWN, Direction.UP, Direction.NORTH)))
-  final val Yaw = DirectionProperty.create("yaw", Direction.Plane.HORIZONTAL.asInstanceOf[Predicate[Direction]])
+  final val Facing = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL)
+  final val Pitch = DirectionProperty.create("pitch", d => d.getAxis == Direction.Axis.Y || d == Direction.NORTH)
+  final val Yaw = DirectionProperty.create("yaw", Direction.Plane.HORIZONTAL)
 }
