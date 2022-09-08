651b5ae9b49e6611cb834e59cbc22d081450dec2
KosmosPrime <5663514+KosmosPrime@users.noreply.github.com>
2022-09-08 22:52:32 +0200

    Fix tooltips not appearing in GUIs

--- src/main/scala/li/cil/oc/client/KeyBindings.scala
+++ src/main/scala/li/cil/oc/client/KeyBindings.scala
@@ -1,9 +1,11 @@
 package li.cil.oc.client
 
 import li.cil.oc.OpenComputers
+import li.cil.oc.client.gui.traits.InputBuffer
 import net.minecraft.client.Minecraft
 import net.minecraft.client.settings.KeyBinding
 import net.minecraft.client.util.InputMappings
+import net.minecraftforge.client.settings.IKeyConflictContext
 import net.minecraftforge.client.settings.KeyConflictContext
 import net.minecraftforge.client.settings.KeyModifier
 import org.lwjgl.glfw.GLFW
@@ -11,30 +13,44 @@ import org.lwjgl.glfw.GLFW
 import scala.collection.mutable
 
 object KeyBindings {
-  def showExtendedTooltips: Boolean = extendedTooltip.isDown
+  private def isActive(input: InputMappings.Input): Boolean = {
+    val window = Minecraft.getInstance.getWindow.getWindow
+    input.getType match {
+      case InputMappings.Type.MOUSE => GLFW.glfwGetMouseButton(window, input.getValue) == GLFW.GLFW_PRESS
+      case InputMappings.Type.SCANCODE => false // GLFW doesn't have a glfwGetScancode method to test these.
+      case InputMappings.Type.KEYSYM => GLFW.glfwGetKey(window, input.getValue) == GLFW.GLFW_PRESS
+    }
+  }
+
+  def showExtendedTooltips: Boolean = {
+    if (extendedTooltip.isDown) return true
+    // We have to know if the keybind is pressed even if the active screen doesn't pass events.
+    if (!extendedTooltip.getKeyConflictContext.isActive) return false
+    if (extendedTooltip.getKey == InputMappings.UNKNOWN) return false
+    extendedTooltip.getKeyModifier match {
+      // KeyModifier.NONE does not accept pure modifier keys by default, so check for that.
+      case KeyModifier.NONE if KeyModifier.isKeyCodeModifier(extendedTooltip.getKey) => isActive(extendedTooltip.getKey)
+      case mod if mod.isActive(extendedTooltip.getKeyConflictContext) => isActive(extendedTooltip.getKey)
+      case _ => false
+    }
+  }
 
   def isAnalyzeCopyingAddress: Boolean = analyzeCopyAddr.isDown
 
   def getKeyBindingName(keyBinding: KeyBinding) = keyBinding.getTranslatedKeyMessage.getString
 
-  val extendedTooltip = new KeyBinding("key.opencomputers.extendedTooltip", KeyConflictContext.GUI,
-    InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_SHIFT, OpenComputers.Name) {
+  val textInputConflict = new IKeyConflictContext {
+    override def isActive: Boolean = Minecraft.getInstance.screen.isInstanceOf[InputBuffer]
 
-    override def isActiveAndMatches(input: InputMappings.Input): Boolean = {
-      input != InputMappings.UNKNOWN && input.equals(getKey) && isConflictContextAndModifierActive
-    }
-
-    override def isConflictContextAndModifierActive: Boolean = {
-      // KeyModifier.NONE does not accept pure modifier keys by default (except for IN_GAME conflict contexts).
-      val modifierActive = getKeyModifier.isActive(getKeyConflictContext) ||
-        (getKeyModifier == KeyModifier.NONE && KeyModifier.isKeyCodeModifier(getKey))
-      getKeyConflictContext.isActive && modifierActive
-    }
+    override def conflicts(other: IKeyConflictContext): Boolean = this == other
   }
+
+  val extendedTooltip = new KeyBinding("key.opencomputers.extendedTooltip", KeyConflictContext.GUI,
+    InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_SHIFT, OpenComputers.Name)
 
   val analyzeCopyAddr = new KeyBinding("key.opencomputers.analyzeCopyAddress", KeyConflictContext.IN_GAME,
     InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_LEFT_CONTROL, OpenComputers.Name)
 
-  val clipboardPaste = new KeyBinding("key.opencomputers.clipboardPaste", KeyConflictContext.GUI,
+  val clipboardPaste = new KeyBinding("key.opencomputers.clipboardPaste", textInputConflict,
     InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_INSERT, OpenComputers.Name)
 }
