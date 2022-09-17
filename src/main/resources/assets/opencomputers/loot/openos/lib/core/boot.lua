00c5a9b3dd09fd6684402e1491d5e437a6ecdfc1
Adrian Siekierka <kontakt@asie.pl>
2022-09-17 23:27:41 +0200

    set OC version to 1.8.0-snapshot, update changelog

--- src/main/resources/assets/opencomputers/loot/openos/lib/core/boot.lua
+++ src/main/resources/assets/opencomputers/loot/openos/lib/core/boot.lua
@@ -1,7 +1,7 @@
 -- called from /init.lua
 local raw_loadfile = ...
 
-_G._OSVERSION = "OpenOS 1.7.7"
+_G._OSVERSION = "OpenOS 1.8.0-snapshot"
 
 -- luacheck: globals component computer unicode _OSVERSION
 local component = component
