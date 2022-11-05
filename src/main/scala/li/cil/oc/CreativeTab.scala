441a4345bbea059116574d060adfc555b59565e6
KosmosPrime <5663514+KosmosPrime@users.noreply.github.com>
2022-11-05 12:44:08 +0100

    Use simpler ItemGroup constructor

--- src/main/scala/li/cil/oc/CreativeTab.scala
+++ src/main/scala/li/cil/oc/CreativeTab.scala
@@ -5,7 +5,7 @@ import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.util.NonNullList
 
-object CreativeTab extends ItemGroup(-1, OpenComputers.Name) {
+object CreativeTab extends ItemGroup(OpenComputers.Name) {
   private lazy val stack = api.Items.get(Constants.BlockName.CaseTier1).createItemStack(1)
 
   override def makeIcon = stack
